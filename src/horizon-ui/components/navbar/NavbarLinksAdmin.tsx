22ce864afea42a6b2626417722baf513237867d3
Weihan Wang <wang.weihan@gmail.com>
2023-06-17 21:16:13 -0700

    Remove unused code

--- src/horizon-ui/components/navbar/NavbarLinksAdmin.tsx
+++ src/horizon-ui/components/navbar/NavbarLinksAdmin.tsx
@@ -33,18 +33,11 @@ const logoutMod = async function (this: Guard) {
 export default function HeaderLinks(props: { secondary: boolean }) {
 	const { secondary } = props;
 	// Chakra Color Mode
-	const navbarIcon = useColorModeValue('gray.400', 'white');
 	let menuBg = useColorModeValue('white', 'navy.800');
-	const textColor = useColorModeValue('secondaryGray.900', 'white');
-	const textColorBrand = useColorModeValue('brand.700', 'brand.400');
-	const ethColor = useColorModeValue('gray.700', 'white');
-	const ethBg = useColorModeValue('secondaryGray.300', 'navy.900');
-	const ethBox = useColorModeValue('white', 'navy.800');
 	const shadow = useColorModeValue(
 		'14px 17px 40px 4px rgba(112, 144, 176, 0.18)',
 		'14px 17px 40px 4px rgba(112, 144, 176, 0.06)'
 	);
-	const borderButton = useColorModeValue('secondaryGray.500', 'whiteAlpha.200');
 
 	const guard = useGuard();
 	const [user] = useUserContext();
@@ -58,61 +51,11 @@ export default function HeaderLinks(props: { secondary: boolean }) {
 			flexWrap={secondary ? { base: 'wrap', md: 'nowrap' } : 'unset'}
 			p='10px'
 			borderRadius='30px'
-			boxShadow={shadow}>
-			<Flex
-				bg={ethBg}
-				display={secondary ? 'flex' : 'none'}
-				borderRadius='30px'
-				ms='auto'
-				p='6px'
-				align='center'
-				me='6px'>
-				<Flex align='center' justify='center' bg={ethBox} h='29px' w='29px' borderRadius='30px' me='7px'>
-					<Icon color={ethColor} w='9px' h='14px' as={FaEthereum} />
-				</Flex>
-				<Text w='max-content' color={ethColor} fontSize='sm' fontWeight='700' me='6px'>
-					1,924
-					<Text as='span' display={{ base: 'none', md: 'unset' }}>
-						{' '}
-						ETH
-					</Text>
-				</Text>
-			</Flex>
+			boxShadow={shadow}
+    >
 			<SidebarResponsive routes={
 				routes.filter(r => isPermitted(user.roles, r.resource))
 			} />
-			<Menu>
-				<MenuButton p='0px'>
-					<Icon mt='6px' as={MdNotificationsNone} color={navbarIcon} w='18px' h='18px' me='10px' />
-				</MenuButton>
-				<MenuList
-					boxShadow={shadow}
-					p='20px'
-					borderRadius='20px'
-					bg={menuBg}
-					border='none'
-					mt='22px'
-					me={{ base: '30px', md: 'unset' }}
-					minW={{ base: 'unset', md: '400px', xl: '450px' }}
-					maxW={{ base: '360px', md: 'unset' }}>
-					<Flex w='100%' mb='20px'>
-						<Text fontSize='md' fontWeight='600' color={textColor}>
-							提醒
-						</Text>
-						<Text fontSize='sm' fontWeight='500' color={textColorBrand} ms='auto' cursor='pointer'>
-							全部标记为已读
-						</Text>
-					</Flex>
-					<Flex flexDirection='column'>
-						{/* <MenuItem _hover={{ bg: 'none' }} _focus={{ bg: 'none' }} px='0' borderRadius='8px' mb='10px'>
-							<ItemContent info='Horizon UI Dashboard PRO' />
-						</MenuItem>
-						<MenuItem _hover={{ bg: 'none' }} _focus={{ bg: 'none' }} px='0' borderRadius='8px' mb='10px'>
-							<ItemContent info='Horizon Design System Free' />
-						</MenuItem> */}
-					</Flex>
-				</MenuList>
-			</Menu>
 			<Menu>
 				<MenuButton p='0px'>
 					<Avatar
