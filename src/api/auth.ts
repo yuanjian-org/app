78ab2915380f300857bb9ebc4ef5cb3c664fbe2d
Weihan Wang <wang.weihan@gmail.com>
2023-06-23 17:24:10 -0700

    Improve functions

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -7,7 +7,7 @@ import apiEnv from "./apiEnv";
 import { UniqueConstraintError } from "sequelize";
 import { AuthenticationClient } from 'authing-js-sdk';
 import { LRUCache } from 'lru-cache';
-import { sendEmailToAdmin } from './sendgrid';
+import { emailAdminsIgnoreError } from './sendgrid';
 
 const USER_CACHE_TTL_IN_MS = 60 * 60 * 1000
 
@@ -106,7 +106,7 @@ async function findOrCreateUser(clientId: string, email: string): Promise<User> 
     const isAdmin = apiEnv.ASSIGN_ADMIN_ROLE_ON_SIGN_UP.includes(email);
     const roles: [Role] = [isAdmin ? 'ADMIN' : 'VISITOR'];
     console.log(`Creating user ${email} roles ${roles} id ${clientId}`);
-    sendEmailToAdmin(`New user ${email} signed up`);
+    emailAdminsIgnoreError("新用户注册", `新用户 ${email} 注册。`);
     try {
       return await User.create({
         name: "",
--- src/api/sendgrid.ts
+++ src/api/sendgrid.ts
@@ -1,47 +1,35 @@
 import { PersonalizationData } from '@sendgrid/helpers/classes/personalization';
-import mail, { MailDataRequired } from '@sendgrid/mail';
+import mail from '@sendgrid/mail';
 import apiEnv from './apiEnv';
 import User from './database/models/User';
 import { Op } from 'sequelize';
 
 mail.setApiKey(apiEnv.SENDGRID_API_KEY);
 
-export const sendEmail = async (personalization: PersonalizationData[], template_id: string) => {
-
-    console.log(`Sending mail via SendGrid: ${JSON.stringify(personalization, null, 2)}`)
-
-    const message: MailDataRequired = {
-        personalizations: personalization,
-        // change to email linked to API key, otherwise would cause error
-        from: 'yuanjian@email.com', 
-        templateId: template_id,
-    }
-
-    await mail.send(message);
-
+/**
+ * Send email using SendGrid API. See https://docs.sendgrid.com/api-reference/mail-send/mail-send for parameter details.
+ */
+export async function email(template_id: string, personalization: PersonalizationData[]) {
+  console.log(`Sending mail via SendGrid: ${JSON.stringify(personalization, null, 2)}`)
+  await mail.send({
+    personalizations: personalization,
+    from: apiEnv.SENDGRID_FROM_EMAIL,
+    templateId: template_id,
+  });
 }
 
-export const sendEmailToAdmin = async (new_user_email: string) => {
-    const template_id = 'd-2e3aabdb9a3c45a393877596534c57c2'; // temporary holder, change to required template id
-
-    const admin = await User.findAll({
-        where: {
-            roles: { [Op.contains]: ["ADMIN"] },
-        }
+export async function emailAdminsIgnoreError(subject: string, content: string) {
+  try {
+    const admins = await User.findAll({
+      where: {
+        roles: { [Op.contains]: ["ADMIN"] },
+      }
     })
-
-    const toSend = admin.map(({ name, email }) => ({ name, email }))
-
-    const personalizations: PersonalizationData[] = [{
-        to: toSend,
-        // edit accordingly on the template parameters
-        dynamicTemplateData: {
-            receiver: 'Admin',
-            new_user_email: new_user_email,
-            sender: 'Yuanjian Admin Group',
-        },
-    }];
-
-    await sendEmail(personalizations, template_id);
-
+    await email('d-99d2ae84fe654400b448f8028238d461', [{
+      to: admins.map(({ name, email }) => ({ name, email })),
+      dynamicTemplateData: { subject, content },
+    }]);
+  } catch (e) {
+    console.log(`Error ignored in emailAdminsIgnoreError("${subject}", "${content}")`, e);
+  }
 }
