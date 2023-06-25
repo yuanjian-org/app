95125b0310c4e03d1fce779a6d81533b4869cc76
Weihan Wang <wang.weihan@gmail.com>
2023-06-24 19:15:55 -0700

    Add a cron job to periodically upload raw transcripts.

---
+++ src/api/routes/cron.ts
@@ -0,0 +1,43 @@
+import { procedure, router } from "../trpc";
+import axios from "axios";
+import apiEnv from "api/apiEnv";
+
+/**
+ * These API are to be periodically triggered for background operations and housekeeping.
+ */
+const cron = router({
+
+  /**
+   * Download transcripts and then upload them as summaries as is.
+   */
+  uploadRawTranscripts: procedure.query(async () => {
+    const baseUrl = `https://beta.yuanjian.org/api/v1/`;
+    const headers = { 'Authorization': `Bearer ${apiEnv.INTEGRATION_AUTH_TOKEN}` };
+  
+    console.log('Retriving transcript URLs...');
+    const res = await axios.get(baseUrl + 'transcripts.list', { headers });
+  
+    const promises = res.data.result.data.map(async (transcript: any) => {
+      const id = transcript.transcriptId;
+      console.log(`Downloading ${id}...`);
+      try {
+        const res = await axios.get(transcript.url);
+        console.log(`Uploading ${id}...`);
+        try {
+          await axios.post(baseUrl + 'summaries.write', { 
+            transcriptId: id,
+            summaryKey: '原始文字（仅试验期用）',
+            summary: res.data,
+          }, { headers });
+        } catch (e) {
+          console.error(`Error uploading ${id}. Ignored:`, (e as Error).message);
+        }
+      } catch (e) {
+        console.error(`Error downloading ${id}. Ignored:`, (e as Error).message);
+      }
+    });
+    await Promise.all(promises);
+  }),
+});
+
+export default cron;
