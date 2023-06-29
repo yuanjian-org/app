5e56741b5ab287fe0953a41456f11f4045d42444
Yichi Zhang <83266861+UncleBu1728@users.noreply.github.com>
2023-06-29 00:52:07 -0500

    Count concurrent meetings

--- src/api/routes/cron.ts
+++ src/api/routes/cron.ts
@@ -1,6 +1,9 @@
 import { procedure, router } from "../trpc";
 import axios from "axios";
 import apiEnv from "api/apiEnv";
+import { listMeetings } from "api/TencentMeeting";
+import OngoingMeetingCount from "api/database/models/OngoingMeetingCount";
+import invariant from "tiny-invariant";
 
 /**
  * These API are to be periodically triggered for background operations and housekeeping.
@@ -14,10 +17,10 @@ const cron = router({
   .query(async () => {
     const baseUrl = `https://beta.yuanjian.org/api/v1/`;
     const headers = { 'Authorization': `Bearer ${apiEnv.INTEGRATION_AUTH_TOKEN}` };
-  
+
     console.log('Retriving transcript URLs...');
     const res = await axios.get(baseUrl + 'transcripts.list', { headers });
-  
+
     const promises = res.data.result.data.map(async (transcript: any) => {
       const id = transcript.transcriptId;
       console.log(`Downloading ${id}...`);
@@ -25,7 +28,7 @@ const cron = router({
         const res = await axios.get(transcript.url);
         console.log(`Uploading ${id}...`);
         try {
-          await axios.post(baseUrl + 'summaries.write', { 
+          await axios.post(baseUrl + 'summaries.write', {
             transcriptId: id,
             summaryKey: '原始文字（仅试验期用）',
             summary: res.data,
@@ -39,6 +42,19 @@ const cron = router({
     });
     await Promise.all(promises);
   }),
+
+  /**
+   * Check the ongoing meetings and update the OngoingMeetingCount table
+   */
+  updateOngoingMeetingCounts: procedure.mutation(async () => {
+    const count = (await listMeetings()).meeting_info_list.filter(obj => obj.status === 'MEETING_STATE_STARTED').length;
+
+    OngoingMeetingCount.upsert({
+      TMAdminUserId: apiEnv.TM_ADMIN_USER_ID,
+      count,
+    })
+  })
+
 });
 
 export default cron;
