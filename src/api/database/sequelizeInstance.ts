a53ac878b09d846d9d4100ce6ad7b0ca60887e3f
Weihan Wang <wang.weihan@gmail.com>
2023-06-30 15:24:13 -0700

    Disable Sequelize logging

--- src/api/database/sequelizeInstance.ts
+++ src/api/database/sequelizeInstance.ts
@@ -10,6 +10,7 @@ import OngoingMeetingCount from "./models/OngoingMeetingCount";
 
 const sequelizeInstance = new Sequelize(apiEnv.DATABASE_URI, {
   models: [User, Group, GroupUser, Transcript, Summary, OngoingMeetingCount],
+  logging: false,
   dialectModule: require('pg'),
   retry: {
     // Error types: https://sequelize.org/api/v6/identifiers.html#errors
