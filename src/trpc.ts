6950ce5cbe962f00eb341d96b14b62fa03cccaf6
Weihan Wang <wang.weihan@gmail.com>
2023-07-01 17:52:39 -0700

    Refactor: converge trpc.ts and trpcNext.ts

--- src/trpc.ts
+++ src/trpc.ts
@@ -3,33 +3,30 @@ import type { ApiRouter } from './api/apiRouter';
 import { requestFinishLink } from "./requestFinishLink";
 
 function getBaseUrl() {
-  if (typeof window !== 'undefined')
-    // browser should use relative path
-    return '';
-  if (process.env.VERCEL_URL)
-    // reference for vercel.com
-    return `https://${process.env.VERCEL_URL}`;
-  if (process.env.RENDER_INTERNAL_HOSTNAME)
-    // reference for render.com
-    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
-  // assume localhost
+  // browser should use relative path
+  if (typeof window !== 'undefined') return '';
+  // vercel.com
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  // render.com
+  if (process.env.RENDER_INTERNAL_HOSTNAME) return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
+  // fall back to localhost    
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
 
-const trpc = createTRPCProxyClient<ApiRouter>({
-  links: [
-    ...(process.env.NODE_ENV === "production" ? [] : [loggerLink()]),
-    requestFinishLink(),
-    httpBatchLink({
-      url: getBaseUrl() + '/api/v1',
-      headers: () => {
-        return {
-          Authorization: `Bearer ${localStorage.getItem('_authing_token')}`,
-        };
-      },
-      maxURLLength: 2083, // a suitable size
-    }),
-  ]
-});
+export const links = [
+  ...(process.env.NODE_ENV === "production" ? [] : [loggerLink()]),
+  requestFinishLink(),
+  httpBatchLink({
+    url: getBaseUrl() + '/api/v1',
+    headers: () => {
+      return {
+        Authorization: `Bearer ${localStorage.getItem('_authing_token')}`,
+      };
+    },
+    maxURLLength: 2083, // a suitable size
+  }),
+];
+
+const trpc = createTRPCProxyClient<ApiRouter>({ links });
 
 export default trpc;
--- src/trpcNext.ts
+++ src/trpcNext.ts
@@ -1,48 +1,16 @@
-import { httpBatchLink } from '@trpc/client';
 import type { ApiRouter } from './api/apiRouter';
 import { createTRPCNext } from "@trpc/next";
-function getBaseUrl() {
-  if (typeof window !== 'undefined')
-    // browser should use relative path
-    return '';
-  if (process.env.VERCEL_URL)
-    // reference for vercel.com
-    return `https://${process.env.VERCEL_URL}`;
-  if (process.env.RENDER_INTERNAL_HOSTNAME)
-    // reference for render.com
-    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
-  // assume localhost
-  return `http://localhost:${process.env.PORT ?? 3000}`;
-}
+import { links } from './trpc';
 
 const trpcNext = createTRPCNext<ApiRouter>({
     config({ ctx }) {
       return {
-        links: [
-          httpBatchLink({
-            /**
-             * If you want to use SSR, you need to use the server's full URL
-             * @link https://trpc.io/docs/ssr
-             **/
-            url: `${getBaseUrl()}/api/v1`,
-            headers: () => {
-              // console.log('getting headers', localStorage.getItem('_authing_token'));
-              return {
-                Authorization: `Bearer ${localStorage.getItem('_authing_token')}`,
-              };
-            },
-            maxURLLength: 2083, // a suitable size
-          }),
-        ],
-        /**
-         * @link https://tanstack.com/query/v4/docs/reference/QueryClient
-         **/
+        links,
+        // https://tanstack.com/query/v4/docs/reference/QueryClient
         // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
       };
     },
-    /**
-     * @link https://trpc.io/docs/ssr
-     **/
+    // https://trpc.io/docs/ssr
     ssr: false,
 });
 
