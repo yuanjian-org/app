74cd26ef8b2e79af89455ed67194346528111676
Weihan Wang <wang.weihan@gmail.com>
2023-07-01 21:17:46 -0700

    Add `baseUrl` to all SendGrid dyamicTemplateData

--- src/api/sendgrid.ts
+++ src/api/sendgrid.ts
@@ -10,16 +10,26 @@ mail.setApiKey(apiEnv.SENDGRID_API_KEY);
 /**
  * Send email using SendGrid API. See https://docs.sendgrid.com/api-reference/mail-send/mail-send for parameter details.
  */
-export async function email(template_id: string, personalization: PersonalizationData[]) {
-  console.log(`Sending mail via SendGrid: ${JSON.stringify(personalization, null, 2)}`)
+export async function email(templateId: string, personalization: PersonalizationData[], baseUrl: string) {
+  // Always attach `base_url` as dynamic template data
+  const ps: any[] = structuredClone(personalization);
+  for (const p of ps) {
+    if ('dynamicTemplateData' in p) {
+      p.dynamicTemplateData.baseUrl = baseUrl;
+    } else {
+      p.dynamicTemplateData = { baseUrl: baseUrl };
+    }
+  }
+
+  console.log(`Sending mail via SendGrid: ${JSON.stringify(ps, null, 2)}`);
   await mail.send({
-    personalizations: personalization,
+    personalizations: ps,
     from: apiEnv.SENDGRID_FROM_EMAIL,
-    templateId: template_id,
+    templateId,
   });
 }
 
-export async function emailAdminsIgnoreError(subject: string, content: string) {
+export async function emailUserManagersIgnoreError(subject: string, content: string, baseUrl: string) {
   try {
     // Use type system to capture typos.
     const role : Role = "UserManager";
@@ -31,7 +41,7 @@ export async function emailAdminsIgnoreError(subject: string, content: string) {
     await email('d-99d2ae84fe654400b448f8028238d461', [{
       to: admins.map(({ name, email }) => ({ name, email })),
       dynamicTemplateData: { subject, content },
-    }]);
+    }], baseUrl);
   } catch (e) {
     console.log(`Error ignored in emailAdminsIgnoreError("${subject}", "${content}")`, e);
   }
