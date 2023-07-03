eac89d022e61b347a46969528e825bd3694a8c6b
Weihan Wang <wang.weihan@gmail.com>
2023-07-02 17:50:25 -0700

    Reformat Footer.tsx. Replace tabs.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,63 +8,63 @@ import Image from "next/image";
 export const bodyFooterSpacing = 80;
 
 export default function Footer() {
-	const textColor = useColorModeValue('gray.400', 'white');
-	return (
-		<Flex
-			zIndex='3'
-			flexDirection={{
-				base: 'column',
-				xl: 'row'
-			}}
-			alignItems={{
-				base: 'center',
-				xl: 'start'
-			}}
-			justifyContent='space-between'
-			paddingX={{ base: '30px', md: '50px' }}
-			paddingBottom='30px'
+  const textColor = useColorModeValue('gray.400', 'white');
+  return (
+    <Flex
+      zIndex='3'
+      flexDirection={{
+        base: 'column',
+        xl: 'row'
+      }}
+      alignItems={{
+        base: 'center',
+        xl: 'start'
+      }}
+      justifyContent='space-between'
+      paddingX={{ base: '30px', md: '50px' }}
+      paddingBottom='30px'
       paddingTop={`${bodyFooterSpacing}px`}
     >
-			<Text
-				color={textColor}
-				textAlign={{
-					base: 'center',
-					xl: 'start'
-				}}
-				paddingBottom={{ base: '20px', xl: '0px' }}>
-				{' '}
-				<Text as='span' fontWeight='500' marginStart='4px'>
-					&copy; {new Date().getFullYear()} 远见教育基金会
-				</Text>
-			</Text>
-			<List display='flex'>
-				<ListItem
-					marginEnd={{
-						base: '20px',
-						md: '44px'
-					}}>
-					<Link fontWeight='500' color={textColor} target='_blank' href='http://app.yuanjian.org'>
-						资源库
-					</Link>
-				</ListItem>
-				<ListItem
-					marginEnd={{
-						base: '20px',
-						md: '44px'
-					}}>
-					<Link fontWeight='500' color={textColor} target='_blank' href='http://yuanjian.org'>
-						官网
-					</Link>
-				</ListItem>
-				<ListItem
-					marginEnd={{
-						base: '20px',
-						md: '44px'
-					}}>
-					<Link fontWeight='500' color={textColor} target='_blank' href='http://yuanjian.org/blog'>
-						博客
-					</Link>
-				</ListItem>
+      <Text
+        color={textColor}
+        textAlign={{
+          base: 'center',
+          xl: 'start'
+        }}
+        paddingBottom={{ base: '20px', xl: '0px' }}>
+        {' '}
+        <Text as='span' fontWeight='500' marginStart='4px'>
+          &copy; {new Date().getFullYear()} 远见教育基金会
+        </Text>
+      </Text>
+      <List display='flex'>
+        <ListItem
+          marginEnd={{
+            base: '20px',
+            md: '44px'
+          }}>
+          <Link fontWeight='500' color={textColor} target='_blank' href='http://app.yuanjian.org'>
+            资源库
+          </Link>
+        </ListItem>
+        <ListItem
+          marginEnd={{
+            base: '20px',
+            md: '44px'
+          }}>
+          <Link fontWeight='500' color={textColor} target='_blank' href='http://yuanjian.org'>
+            官网
+          </Link>
+        </ListItem>
+        <ListItem
+          marginEnd={{
+            base: '20px',
+            md: '44px'
+          }}>
+          <Link fontWeight='500' color={textColor} target='_blank' href='http://yuanjian.org/blog'>
+            博客
+          </Link>
+        </ListItem>
         <Center opacity='50%'>
           <ListItem>
               <Link target='_blank' href="https://vercel.com/?utm_source=yuanjian&utm_campaign=oss">
@@ -72,7 +72,7 @@ export default function Footer() {
               </Link>
           </ListItem>
         </Center>
-			</List>
-		</Flex>
-	);
+      </List>
+    </Flex>
+  );
 }
