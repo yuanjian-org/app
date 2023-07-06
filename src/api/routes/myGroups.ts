fd6c92bbee0c5d15ae95b6433cb07f0e98fd0499
Yichi Zhang <yizhang1728@gmail.com>
2023-07-05 22:33:36 -0500

    updated myGroup and added warn modal

--- src/api/routes/myGroups.ts
+++ src/api/routes/myGroups.ts
@@ -12,19 +12,23 @@ import { createMeeting } from "../TencentMeeting";
 import Transcript from "../database/models/Transcript";
 import moment from 'moment';
 import { noPermissionError, notFoundError, zGroupCountingTranscripts } from "./groups";
+import OngoingMeetingCount from "api/database/models/OngoingMeetingCount";
+import apiEnv from "api/apiEnv";
 
 export function meetingLinkIsExpired(meetingLinkCreatedAt : Date) {
-  // meeting is valid for 31 days after start time
+  // meeting is valid for 30 days after start time
   // check if the link is created within 30 days
   return moment() > moment(meetingLinkCreatedAt).add(30, 'days');
 }
 
 const myGroups = router({
 
-  generateMeetingLink: procedure
+  joinMeetingLink: procedure
   .use(authUser())
   .input(z.object({ groupId: z.string() }))
   .mutation(async ({ ctx, input }) => {
+    let meetingLink : string | null;
+    let meetingId: string | null;
     const group = await Group.findByPk(input.groupId, {
       include: [GroupUser]
     });
@@ -32,23 +36,42 @@ const myGroups = router({
     // Only meeting members have access to this method.
     if (!group.groupUsers.some(gu => gu.userId === ctx.user.id)) throw noPermissionError(input.groupId);
 
-    if (group.meetingLink && !meetingLinkIsExpired(group.updatedAt)) {
-      return group.meetingLink;
+    if (group.meetingLinkCreatedAt && !meetingLinkIsExpired(group.meetingLinkCreatedAt)) {
+      meetingLink = group.meetingLink;
+      meetingId = group.meetingId;
+    } else {
+
+      const now = Math.floor(Date.now() / 1000);
+      const res = await createMeeting(group.id, now, now + 3600);
+      invariant(res.meeting_info_list.length === 1);
+
+      meetingLink = res.meeting_info_list[0].join_url;
+      meetingId = res.meeting_info_list[0].meeting_id;
+
+      await Group.update({
+        meetingLink,
+        meetingId,
+        meetingLinkCreatedAt: moment.unix(now).toDate(),
+      }, {
+        where: {
+          id: group.id,
+        }
+      });
     }
 
-    const now = Math.floor(Date.now() / 1000);
-    const res = await createMeeting(group.id, now, now + 3600);
-    invariant(res.meeting_info_list.length === 1);
+    const ongoingMeeting = await OngoingMeetingCount.findByPk(apiEnv.TM_ADMIN_USER_ID);
+    // Check if the meeting id of the ongoingMeeting matches the meeting id of the group tries to join
+    // The group user will allow to join the meeting if matched
+    // otherwise return null to warn that user cannot join right now due to other ongoing meetings
+    if (ongoingMeeting && ongoingMeeting.count !== 0) {
+      return ongoingMeeting.meetingId === meetingId ? meetingLink : null;
+    }
 
-    const meetingLink = res.meeting_info_list[0].join_url;
-
-    await Group.update({
-      meetingLink,
-    }, {
-      where: {
-        id: group.id,
-      }
-    });
+    OngoingMeetingCount.upsert({
+      TMAdminUserId: apiEnv.TM_ADMIN_USER_ID,
+      count: 1,
+      meetingId,
+    })
 
     return meetingLink;
   }),
--- src/components/GroupBar.tsx
+++ src/components/GroupBar.tsx
@@ -5,6 +5,13 @@ import {
   Center,
   Flex,
   HStack,
+  Modal,
+  ModalHeader,
+  ModalContent,
+  ModalCloseButton,
+  ModalOverlay,
+  ModalBody,
+  ModalFooter,
   SimpleGrid,
   Spacer,
   Text,
@@ -18,6 +25,7 @@ import { MdChevronRight, MdVideocam } from 'react-icons/md';
 import Link from 'next/link';
 import useUserContext from 'useUserContext';
 import { formatGroupName } from 'shared/formatNames';
+import ModalWithBackdrop from './ModalWithBackdrop';
 
 // @ts-ignore TODO: fix me.
 export default function GroupBar(props: {
@@ -30,11 +38,17 @@ export default function GroupBar(props: {
   const [user] = useUserContext();
   const transcriptCount = (props.group.transcripts || []).length;
   const [isJoiningMeeting, setJoining] = useState(false);
+  const [hasMeetingQuota, setHasMeetingQuota] = useState(false);
   const launchMeeting = async (groupId: string) => {
     setJoining(true);
     try {
-      const link = await trpc.myGroups.generateMeetingLink.mutate({ groupId: groupId });
-      window.location.href = link;
+      const link = await trpc.myGroups.joinMeetingLink.mutate({ groupId: groupId });
+      if (!link) {
+        setHasMeetingQuota(true);
+        setJoining(false);
+      } else {
+        window.location.href = link;
+      }
     } catch (e) {
       // See comments in the `finally` block below.
       setJoining(false);
@@ -52,6 +66,7 @@ export default function GroupBar(props: {
       templateColumns={(props.showJoinButton ? '6em ' : '') + '1fr'}
       spacing={4}
     >
+      {hasMeetingQuota && <OngoingMeetingWarning onClose={() => setHasMeetingQuota(false)}/>}
       {/* row 1 col 1 */}
       {props.showJoinButton && <Box />}
 
@@ -101,6 +116,28 @@ export default function GroupBar(props: {
         </>}
       </Flex>
     </SimpleGrid>
+  );
+}
+
+function OngoingMeetingWarning(props: {
+  onClose: () => void,
+}) {
+  return (<ModalWithBackdrop isOpen onClose={props.onClose}>
+    <ModalOverlay />
+    <ModalContent>
+      <ModalHeader>当前无法加入会议</ModalHeader>
+      <ModalCloseButton />
+      <ModalBody>
+        <p>腾讯在线会议上限已满，请稍后再试</p>
+        <p>如有特殊请求请联系管理员</p>
+      </ModalBody>
+      <ModalFooter>
+        <Button mr={4} onClick={props.onClose}>
+          确认
+        </Button>
+      </ModalFooter>
+    </ModalContent>
+  </ModalWithBackdrop>
   );
 }
 
