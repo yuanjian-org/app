48d756d550a60a05d1103c4e1c1a8c5ff504c0e8
Fangkuaikuai <cuberluziyi@gmail.com>
2023-07-11 15:11:52 -0500

    improved parseSpeakerStats

---
+++ src/api/parseSpeakerStats.ts
@@ -0,0 +1,64 @@
+export interface SpeakerStats {
+  name: string;
+  totalSpeakingSeconds: number;
+}
+
+export function parseSpeakerStats(data: string): SpeakerStats[] {
+  const speakerTimes: Map<string, number> = new Map();
+  const lines = data.split('\n');
+  const pattern = /^(.+?)\((\d{2}):(\d{2}):(\d{2})\):/;
+  
+  let lastTimestamp: number | null = null;
+  let lastSpeaker: string | null = null;
+
+  for (const line of lines) {
+    const match = line.match(pattern);
+
+    if (match) {
+      const speaker = match[1];
+      const hours = Number(match[2]);
+      const minutes = Number(match[3]);
+      const seconds = Number(match[4]);
+
+      // discard invalid timestamp
+      if (minutes >= 60 || seconds >= 60) {
+        continue;
+      }
+
+      const currentTimestamp = hours * 3600 + minutes * 60 + seconds;
+
+      if (lastSpeaker && lastTimestamp !== null) {
+        // discard records in wrong order
+        if (currentTimestamp < lastTimestamp) {
+          continue;  
+        }
+
+        const speakingTime = speakerTimes.get(lastSpeaker) || 0;
+        speakerTimes.set(lastSpeaker, speakingTime + currentTimestamp - lastTimestamp);
+      }
+
+      lastSpeaker = speaker;
+      lastTimestamp = currentTimestamp;
+    }
+    // when last speaker only shows once in whole string
+     if (lastSpeaker && !speakerTimes.has(lastSpeaker)) {
+      speakerTimes.set(lastSpeaker, 0);
+    }
+  }
+
+  const speakerStats: SpeakerStats[] = Array.from(speakerTimes.entries()).map(([name, totalSpeakingSeconds]) => ({
+    name, totalSpeakingSeconds
+  }));
+
+  return speakerStats;
+}
+
+// normal test case
+const data = `
+Speaker A(00:00:10): Hello!
+Speaker B(00:00:15): Hi!
+Speaker A(00:00:20): How are you?
+`;
+
+const speakerStats = parseSpeakerStats(data);
+console.log(speakerStats);
