9360b04fef407291bab8cd7c44f59aebb9970323
Weihan Wang <wang.weihan@gmail.com>
2023-07-19 21:03:04 -0700

    Refactor: Reformat cron.ts

--- src/api/routes/cron.ts
+++ src/api/routes/cron.ts
@@ -5,36 +5,33 @@ import { findMissingCrudeSummaries, saveCrudeSummary } from "./summaries";
 import { updateOngoingMeetings } from "./meetings";
 
 /**
- * These API are to be periodically triggered for background operations and housekeeping.
+ * Download crude summaries from Tencent Meeting and save them locally.
+ * 
+ * @returns the transcriptIds of synced transcripts.
  */
-const cron = router({
-
-  /**
-   * Download crude summaries from Tencent Meeting and save them locally.
-   * 
-   * @returns the transcriptIds of synced transcripts.
-   */
-  syncCrudeSummaries: procedure
+const syncCrudeSummaries = procedure
   .output(z.object({
     syncedCrudeSummaries: z.array(z.string()),
   }))
-  .mutation(async () => {
-    console.log('Looking for missing crude summaries...');
-    const summaries = await findMissingCrudeSummaries();
+  .mutation(async () => 
+{
+  console.log('Looking for missing crude summaries...');
+  const summaries = await findMissingCrudeSummaries();
 
-    const promises = summaries.map(async summary => {
-      console.log(`Downloading ${summary.transcriptId}...`);
-      const res = await axios.get(summary.url);
-      await saveCrudeSummary(summary, res.data);
-    });
-    await Promise.all(promises);
+  const promises = summaries.map(async summary => {
+    console.log(`Downloading ${summary.transcriptId}...`);
+    const res = await axios.get(summary.url);
+    await saveCrudeSummary(summary, res.data);
+  });
+  await Promise.all(promises);
 
-    return {
-      syncedCrudeSummaries: summaries.map((t: any) => t.transcriptId),
-    }
-  }),
-
-  updateOngoingMeetings: procedure.mutation(updateOngoingMeetings)
+  return {
+    syncedCrudeSummaries: summaries.map((t: any) => t.transcriptId),
+  }
 });
 
+const cron = router({
+  syncCrudeSummaries,
+  updateOngoingMeetings: procedure.mutation(updateOngoingMeetings),
+});
 export default cron;
