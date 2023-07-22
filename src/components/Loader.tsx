089cef86fe9cb675b9ce377472c81b3a0eff4023
Qianrui Zhao <qz2338@columbia.edu>
2023-07-21 22:55:13 -0700

    UI improvmeents on profile.tsx (#106)

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@chakra-ui/react";
 
-export default function Loader() {
-  return <Button isLoading={true} loadingText={'加载中...'} disabled variant="ghost" />;
+export default function Loader(props: {
+  loadingText?: string,
+}) {
+  return <Button isLoading={true} loadingText={props.loadingText ? props.loadingText : '加载中...'} disabled variant="ghost" />;
 }
