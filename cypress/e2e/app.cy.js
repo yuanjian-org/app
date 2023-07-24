d364ed2f4b6a8b1b7f6c281f72c6aae11718f73f
Weihan Wang <wang.weihan@gmail.com>
2023-07-23 22:46:25 -0700

    Minor updates to cypress config and test files.

--- cypress/e2e/app.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Navigation', () => {
-  it('land on login page', () => {
-    cy.visit('/')
-    
-    cy.url().should('include', '/login')
-    cy.get('#authing-guard-container').contains('testing')
-  })
-})
