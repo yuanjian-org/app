f0966f20452475c47d0c105855ac0f636725b3d3
Weihan Wang <wang.weihan@gmail.com>
2023-07-23 23:37:01 -0700

    Use test.ts suffix for Cypress test files to avoid compiler errors

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'cypress'
 
+/**
+ * Example: https://github.com/cypress-io/cypress-realworld-app/blob/develop/cypress.config.js
+ */
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    defaultCommandTimeout: 10000
+    defaultCommandTimeout: 10000,
+    specPattern: "cypress/e2e/**/*.test.{ts,tsx}",
   },
 });
