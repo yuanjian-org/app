55544d9d89f6e2a295dbd3e06337507e16ee6a0e
Fangkuaikuai <cuberluziyi@gmail.com>
2023-07-26 14:19:10 +0800

    fix typo

--- src/api/database/models/User.ts
+++ src/api/database/models/User.ts
@@ -4,6 +4,8 @@ import type {
 } from "sequelize";
 import {
   AllowNull,
+  BeforeBulkDestroy,
+  BeforeDestroy,
   Column,
   Index,
   Table,
@@ -11,11 +13,14 @@ import {
 } from "sequelize-typescript";
 import Fix from "../modelHelpers/Fix";
 import ParanoidModel from "../modelHelpers/ParanoidModel";
-import { DATE, JSONB, STRING } from "sequelize";
+import { DATE, JSONB, Op, STRING } from "sequelize";
 import ZodColumn from "../modelHelpers/ZodColumn";
 import Role, { zRoles } from "../../../shared/Role";
 import z from "zod";
 import { toPinyin } from "../../../shared/strings";
+import GroupUser from "./GroupUser";
+import Partnership from "./Partnership";
+
 
 @Table({ tableName: "users", modelName: "user" })
 @Fix
@@ -55,6 +60,22 @@ class User extends ParanoidModel<
 
   @ZodColumn(JSONB, z.record(z.string(), z.any()).nullable())
   menteeApplication: object | null;
+
+  @BeforeDestroy
+  static async cascadeDestory( user: User, options: any){
+    const promises1 = (await GroupUser.findAll({
+      where: { userId: user.id }
+    })).map( async gu => {await gu.destroy(options); });
+
+    const promises2 = (await Partnership.findAll({
+      where: { 
+        [Op.or]: [{menteeId: user.id}, {mentorId: user.id}] 
+      }
+    })).map(async p => { await p.destroy(options); });
+    
+    await Promise.all([...promises1, promises2]);
+  }
+  
 }
 
 export default User;
