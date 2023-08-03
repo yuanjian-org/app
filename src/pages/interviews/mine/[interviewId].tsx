69d50f735da6648a5ac1559f2205248648687743
Weihan Wang <wang.weihan@gmail.com>
2023-08-03 14:39:25 -0700

    Add interviewers instructions

--- src/pages/interviews/mine/[interviewId].tsx
+++ src/pages/interviews/mine/[interviewId].tsx
@@ -12,8 +12,13 @@ import { Flex, Grid, GridItem,
   SliderThumb,
   SliderMark,
   Tooltip,
+  Icon,
+  Link,
+  UnorderedList,
+  ListItem,
+  Text,
+  Center,
 } from '@chakra-ui/react';
-import GroupBar from 'components/GroupBar';
 import { sidebarBreakpoint } from 'components/Navbars';
 import { useUserContext } from 'UserContext';
 import invariant from "tiny-invariant";
@@ -26,6 +31,9 @@ import Autosaver from 'components/Autosaver';
 import { InterviewFeedback } from 'shared/InterviewFeedback';
 import _ from "lodash";
 import MenteeApplication from 'components/MenteeApplication';
+import { BsWechat } from "react-icons/bs";
+import { MinUser } from 'shared/User';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 const Page: NextPageWithLayout = () => {
   const interviewId = parseQueryParameter(useRouter(), 'interviewId');
@@ -37,11 +45,14 @@ const Page: NextPageWithLayout = () => {
     <PageBreadcrumb current={formatUserName(interview.interviewee.name, "formal")} parents={[{
       name: "我的面试", link: "/interviews/mine",
     }]}/>
-    <GroupBar group={interview.group} showJoinButton showGroupName={false} marginBottom={8} />
+    {/* <GroupBar group={interview.group} showGroupName={false} showJoinButton marginBottom={8} /> */}
     {/* TODO: For some reason "1fr 1fr" doens't work */}
     <Grid templateColumns={{ base: "100%", [sidebarBreakpoint]: "40% 50%" }} gap={10}>
       <GridItem>
-        <FeedbackEditor interview={interview} />
+        <Flex direction="column" gap={10}>
+          <Instructions interviewers={interview.feedbacks.map(f => f.interviewer)} />
+          <FeedbackEditor interview={interview} />
+        </Flex>
       </GridItem>
       <GridItem>
         {interview.type == "MenteeInterview" ? <MenteeApplication menteeUserId={interview.interviewee.id} /> : <Box />}
@@ -53,6 +64,48 @@ const Page: NextPageWithLayout = () => {
 Page.getLayout = (page) => <AppLayout unlimitedPageWidth>{page}</AppLayout>;
 
 export default Page;
+
+function Instructions({ interviewers }: {
+  interviewers: MinUser[],
+}) {
+  const [me] = useUserContext();
+
+  let first: boolean | null = null;
+  let other: MinUser | null = null;
+  invariant(interviewers.filter(i => i.id === me.id).length == 1);
+  if (interviewers.length == 2) {
+    other = interviewers[0].id === me.id ? interviewers[1] : interviewers[0];
+    first = other.id > me.id;
+  }
+
+  const marginEnd = 1.5;
+  return <Flex direction="column" gap={6}>
+    {/* <b>面试官必读</b> */}
+    <UnorderedList>
+      <ListItem>用<Icon as={BsWechat} marginX={1.5} />微信发起视频群聊。</ListItem>
+      {first !== null && <>
+        <ListItem>
+          <Text as="span" color="red.600">你主持维度 {first ? "1到4" : "5 到 8"} 的问题；</Text>
+          {formatUserName(other?.name ?? null, "friendly")}主持维度 {first ? "5 到 8" : "1 到 4"}。
+        </ListItem>
+        <ListItem><Text color="red.600">填写所有8个维度的评价，以及总评。</Text></ListItem>
+      </>}
+    </UnorderedList>
+    <b>参考资料</b>
+    <UnorderedList>
+    <ListItem>
+        <Link isExternal href="https://www.notion.so/yuanjian/0de91c837f1743c3a3ecdedf78f9e064">
+          面试维度和参考题库 <ExternalLinkIcon />
+        </Link>
+      </ListItem>
+      <ListItem>
+        <Link isExternal href="https://www.notion.so/yuanjian/4616bf621b5b41fbbd62477d66d87ffe">
+          面试须知 <ExternalLinkIcon />
+        </Link>
+      </ListItem>
+    </UnorderedList>
+  </Flex>;
+}
 
 type Feedback = {
   summary: string,
@@ -129,11 +182,12 @@ function FeedbackEditor({ interview }: {
   };
 
   return !interviewFeedback ? <Loader /> : <Flex direction="column" gap={6}>
-    {dimensionNames.map(dn => {
+    {dimensionNames.map((dn, idx) => {
       const d = findDimension(interviewFeedback.feedback as Feedback, dn);
       return <FeedbackDimensionEditor 
         key={dn} 
-        editorKey={`${feedbackId}-${dn}`} 
+        editorKey={`${feedbackId}-${dn}`}
+        index={idx}
         name={dn}
         initialScore={d?.score || defaultScore}
         initialComment={d?.comment || defaultCommentAndSummary}
@@ -154,8 +208,9 @@ function FeedbackEditor({ interview }: {
   </Flex>;
 }
 
-function FeedbackDimensionEditor({ editorKey, name, initialScore, initialComment, onSave }: {
+function FeedbackDimensionEditor({ editorKey, index, name, initialScore, initialComment, onSave }: {
   editorKey: string,
+  index: number,
   name: string,
   initialScore: number,
   initialComment: string,
@@ -169,7 +224,7 @@ function FeedbackDimensionEditor({ editorKey, name, initialScore, initialComment
 
   return <>
     <Flex direction="row" gap={3}>
-      <Box minWidth={120}><b>{name}</b></Box>
+      <Box minWidth={140}><b>{index + 1}. {name}</b></Box>
       <Slider min={1} max={5} step={1} defaultValue={initialScore}
         onChange={setScore}
         onMouseEnter={() => setShowTooltip(true)}
