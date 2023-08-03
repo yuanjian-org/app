981a9aec069eb02f4555dd4b3839d0e7d42cef06
Weihan Wang <wang.weihan@gmail.com>
2023-08-03 11:58:38 -0700

    Support Proxied Mentee Application form

--- src/api/webhooks/submitMenteeApplication.ts
+++ src/api/webhooks/submitMenteeApplication.ts
@@ -6,25 +6,29 @@ import { emailRoleIgnoreError } from "../sendgrid";
 import menteeApplicationFields from "../../shared/menteeApplicationFields";
 
 /**
- * The Webhook for 金数据 submissions form https://jinshuju.net/f/FBTWTe
+ * The Webhook for 金数据 forms https://jinshuju.net/f/FBTWTe (mentee application) and https://jinshuju.net/f/S74k0V
+ * (proxied mentee application).
  */
 export default procedure
   .input(z.record(z.string(), z.any()))
   .mutation(async ({ ctx, input }) => submit(input, ctx.baseUrl));
 
 export async function submit({ form, entry }: Record<string, any>, baseUrl: string) {
-  if (form !== "FBTWTe") {
-    throw generalBadRequestError("This webhook only accepts callbacks from https://jinshuju.net/f/FBTWTe.");
+  if (form !== "FBTWTe" && form !== "S74k0V") {
+    throw generalBadRequestError(`金数据 form id ${form} is not suppoted.`);
   }
 
   const application: Record<string, any> = {};
   for (const field of menteeApplicationFields) {
-    const jn = field.jinshujuName;
+    const jn = form == "FBTWTe" ? field.jsjField : field.jsjProxiedField;
     if (jn && jn in entry) {
       application[field.name] = entry[jn];
     }
   }
 
+  /**
+   * Both forms happen to share the same field names for the following fields. Update code if/when they diverge.
+   */
   const name = entry.field_104;
   await createUser({
     name,
@@ -33,7 +37,7 @@ export async function submit({ form, entry }: Record<string, any>, baseUrl: stri
     wechat: entry.field_106,
     menteeApplication: application,
     roles: ["Mentee"]
-  });
+  }, "upsert");
 
   emailRoleIgnoreError("UserManager", "新学生申请", `姓名：${name}`, baseUrl);
 }
