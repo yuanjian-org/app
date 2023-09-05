b8e760b8767ecca49d145170859fb3f6cb8c5c6d
Yichi Zhang <yizhang1728@gmail.com>
2023-09-05 10:11:17 -0500

    resolve pr 168 comment

--- src/shared/logToSentry.ts
+++ src/shared/logToSentry.ts
@@ -1,25 +1,34 @@
-import { z } from 'zod';
-// other details like timestamp, and hostname/ IP address should be included by Sentry
-const zLogObject = z.object({
-    message: z.string(),
-    email: z.string().email().optional(),
-    data: z.object({}).optional()
-})
-
-enum logLevel{
+enum LogLevel {
     ERROR,
     INFO
-}
+};
 
-export type logObject = z.TypeOf<typeof zLogObject>;
-
-const logToSentry = (tag: keyof typeof logLevel, logDetail: logObject) => {
+// logTosentry is called to use console logger to pass messages to Sentry/Issue as logging service
+// Details like timestamp, and hostname/IP address should be included by Sentry 
+const logToSentry = (tag: keyof typeof LogLevel, message: string, email?: string, data?: string | {}) => {
     if ((tag) === 'ERROR') {
-        console.error('LOG', tag, JSON.stringify(logDetail));
-    }
+        console.error('LOG', tag, message + ", user: " + email, formatData(data));
+    };
 
     if ((tag) === 'INFO') {
-        console.info('LOG', tag, JSON.stringify(logDetail));
+        console.info('LOG', tag, message + ", user: " + email, formatData(data));
+    };
+};
+
+function formatData(data: any | undefined) {
+    if (!data) { return '' };
+    if (typeof data === 'string' || data instanceof String) { return "\nadditional data:" + data };
+    // convert to string if data type is object
+    if (typeof data === 'object') {
+        var str = '';
+        for (var k in data) {
+            if (data.hasOwnProperty(k)) {
+                str += k + ': ' + data[k] + ',' + '\n';
+            }
+        }
+        return "\nadditional data:{ \n" + str + "}";
+    } else {
+        return "ERROR - invalid data Type";
     }
 }
 
