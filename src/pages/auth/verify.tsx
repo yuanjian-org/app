e7c10ce725d3ee882fe82afa262824b3ec4f62bb
Weihan Wang <wang.weihan@gmail.com>
2023-09-16 21:31:08 -0700

    Minor updates
    - minor UI updates
    - support upper case email addresses
    - add one more field to mentee applications

--- src/pages/auth/verify.tsx
+++ src/pages/auth/verify.tsx
@@ -10,18 +10,20 @@ export default function VerifyRequest() {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
+  const email = localStorage.getItem(localStorageKeyForLoginEmail);
+  const callbackUrl = localStorage.getItem(localStorageKeyForLoginCallbackUrl);
+
   const submit = async (token: string) => {
     setIsLoading(true);
     try {
-      const email = localStorage.getItem(localStorageKeyForLoginEmail);
-      const callbackUrl = localStorage.getItem(localStorageKeyForLoginCallbackUrl);
-    
       if (!email || !callbackUrl) {
         console.error("Email or callbackUrl absent from local storage");
         toast.error("糟糕，无法读取浏览器数据。请联系管理员。");
       } else {
-        router.push(`/api/auth/callback/sendgrid?` +
-          `callbackUrl=${encodeURIComponent(callbackUrl)}&token=${token}&email=${encodeURIComponent(email)}`);
+        // next-auth automatically convert all email addresses to lower case.
+        const lower = email.toLowerCase();
+        await router.push(`/api/auth/callback/sendgrid?` +
+          `callbackUrl=${encodeURIComponent(callbackUrl)}&token=${token}&email=${encodeURIComponent(lower)}`);
       }
     } finally {
       setIsLoading(false);
@@ -43,7 +45,7 @@ export default function VerifyRequest() {
     </HStack>
 
     <Text>{' '}</Text>
-    <Text>您应该已经收到了一封带有验证码的邮件。否则请检查垃圾邮箱，或者返回上一页重试。</Text>
+    <Text>验证码已发至 <b>{email}</b>。若未收到邮件，请核对邮箱地址、检查垃圾收件箱，或者返回上一页重试。</Text>
     <Text><Link as={NextLink} href="/">返回重试</Link></Text>
 
     {/* For some reason `opacity=0` doesn't work */}
