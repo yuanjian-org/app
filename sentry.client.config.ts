008adaba009315c10668c688a4e7bc69b6439054
Yichi Zhang <83266861+UncleBu1728@users.noreply.github.com>
2023-09-17 00:58:13 -0500

    Add Logging service using Sentry captureConsole (#168)
    * remove callback
    
    * testing capture logging
    
    * add customized logging function
    
    * update environment
    
    * remove unnecessary file
    
    * restore callback.tsx, will be removed at next PR
    
    * resolve pr 168 comment
    
    * resolve PR168 semicolon
    
    * add formatter to logging function
    
    * resolve conflicts with updates
    
    * revert customized api and add capture console
    
    * revert login page change
    
    * change log level

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,11 +3,15 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import { CaptureConsole } from '@sentry/integrations';
 
 Sentry.init({
   dsn: "https://a1dc8cd72a62aacab0dd520eee207398@o4505687617175552.ingest.sentry.io/4505687622090752",
 
-  integrations: [new Sentry.BrowserTracing()],
+  integrations: [
+    new Sentry.BrowserTracing(),
+    new CaptureConsole({ levels: ['error', 'warn'] }) // using error and info as customized tag logging service
+  ],
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
