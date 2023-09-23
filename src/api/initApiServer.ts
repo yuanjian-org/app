bb6ecc7befa2aa1441c7bd292f4e3ce2e6839893
Weihan Wang <wang.weihan@gmail.com>
2023-09-23 11:50:11 -0700

    Remove initApiServer.ts

--- src/api/initApiServer.ts
+++
@@ -1,7 +0,0 @@
-import sequelize from "./database/sequelize";
-import invariant from "tiny-invariant";
-
-export default function initApiServer() {
-  // ensure database is loaded
-  invariant(sequelize);
-};
