ec7304febaf7febb206e0757865ab8de5a634e59
addlistener <4531670+addlistener@users.noreply.github.com>
2023-11-22 23:53:26 -0800

    moved file to a standalone folder

--- src/pages/chatbot.tsx
+++ src/pages/chatbot.tsx
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic';
 
 // TODO why type mismatch here???
 // @ts-ignore
-const DynamicChatbot = dynamic(() => import('../components/chatbot/components/home'), {
+const DynamicChatbot = dynamic(() => import('../chatbot/components/home'), {
   loading: () => <p>Loading...</p>,
   ssr: false,
 });
