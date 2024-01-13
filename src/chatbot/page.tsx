b3b4308f906bb18c4dc2e043ced52dbb396b9441
addlistener <4531670+addlistener@users.noreply.github.com>
2024-01-12 22:08:39 -0800

    fix tsc errors

--- src/chatbot/page.tsx
+++ src/chatbot/page.tsx
@@ -1,16 +1,16 @@
-import { Analytics } from "@vercel/analytics/react";
+// import { Analytics } from "@vercel/analytics/react";
 
 import { Home } from "./components/home";
 
-import { getServerSideConfig } from "./config/server";
-
-const serverConfig = getServerSideConfig();
+// import { getServerSideConfig } from "./config/server";
+//
+// const serverConfig = getServerSideConfig();
 
 export default async function App() {
   return (
     <>
       <Home />
-      {serverConfig?.isVercel && <Analytics />}
+      {/*{serverConfig?.isVercel && <Analytics />}*/}
     </>
   );
 }
