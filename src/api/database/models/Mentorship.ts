2316da490821c977a385e8054d71b024c9485fdf
Yichi Zhang <yizhang1728@gmail.com>
2024-03-10 22:40:13 -0500

    seperate migration script

--- src/api/database/models/Mentorship.ts
+++ src/api/database/models/Mentorship.ts
@@ -1,34 +1,3 @@
-import { QueryInterface, Sequelize }from 'sequelize';
-
-// Sequelize Migration Script
-module.exports = {
-  up: async (queryInterface: QueryInterface, _: Sequelize) => {
-    const transaction = await queryInterface.sequelize.transaction();
-    try {
-      // transforming Partnership to Mentorship, and partnershipId to mentorshipId
-      await queryInterface.renameTable('Partnership', 'Mentorship', { transaction });
-      await queryInterface.renameColumn('Mentorship', 'partnershipId', 'mentorshipId', { transaction });
-      await transaction.commit();
-    } catch (err) {
-      await transaction.rollback();
-      throw err;
-    }
-  },
-
-  down: async (queryInterface: QueryInterface, _: Sequelize) => {
-    // for reverting the changes
-    const transaction = await queryInterface.sequelize.transaction();
-    try {
-      await queryInterface.renameTable('Mentorship', 'Partnership', { transaction });
-      await queryInterface.renameColumn('Partnership', 'mentorshipId', 'partnershipId', { transaction });
-      await transaction.commit();
-    } catch (err) {
-      await transaction.rollback();
-      throw err;
-    }
-  }
-};
-
 import {
   Column,
   ForeignKey,
--- src/api/database/models/MentorshipAssessment.ts
+++ src/api/database/models/MentorshipAssessment.ts
@@ -1,32 +1,3 @@
-import { QueryInterface, Sequelize }from 'sequelize';
-
-// Sequelize Migration Script
-module.exports = {
-  up: async (queryInterface: QueryInterface, _: Sequelize) => {
-    const transaction = await queryInterface.sequelize.transaction();
-    try {
-      // transforming Assessment to MentorshipAssessment
-      await queryInterface.renameTable('Assessment', 'MentorshipAssessment', { transaction });
-      await transaction.commit();
-    } catch (err) {
-      await transaction.rollback();
-      throw err;
-    }
-  },
-
-  down: async (queryInterface: QueryInterface, _: Sequelize) => {
-    // for reverting the changes
-    const transaction = await queryInterface.sequelize.transaction();
-    try {
-      await queryInterface.renameTable('MentorshipAssessment', 'Assessment', { transaction });
-      await transaction.commit();
-    } catch (err) {
-      await transaction.rollback();
-      throw err;
-    }
-  }
-};
-
 import {
   Column,
   ForeignKey,
