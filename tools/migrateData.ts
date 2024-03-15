11964b1d985b9c4669e25752f38cdc06f2855963
Yichi Zhang <yizhang1728@gmail.com>
2024-03-15 13:00:15 -0500

    remove db queries

--- tools/migrateData.ts
+++ tools/migrateData.ts
@@ -6,12 +6,6 @@ import { exec } from "child_process";
 export default async function migrateData() {
   console.log("Migrating...");
 
-  await sequelize.query('alter table "Partnerships" drop column if exists "coachId"');
-  await sequelize.query('alter table "groups" drop column if exists "coachingPartnershipId"');
-  await sequelize.query('alter table "ChatMessages" drop column if exists "threadId"');
-  await sequelize.query('alter table "Partnerships" drop column if exists "internalChatThreadId"');
-  await sequelize.query('drop table if exists "ChatThreads"');
-
   await new Promise((resolve, reject) => {
     exec('npx sequelize-cli db:migrate --config config/sequelize-cli-config.js', (error, stdout, stderr) => {
       if (error) {
