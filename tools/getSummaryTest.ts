c5cc03411a8662aa4f4a65f599c19a00a116be12
addlistener <4531670+addlistener@users.noreply.github.com>
2024-05-07 11:56:51 +0800

    TencentMeeting getSummary POC

---
+++ tools/getSummaryTest.ts
@@ -0,0 +1,20 @@
+import { loadEnvConfig } from '@next/env';
+import { getChapters, getSummary } from "../src/api/TencentMeeting";
+import apiEnv from "../src/api/apiEnv";
+loadEnvConfig('./');
+
+// listRecords(apiEnv.TM_USER_IDS[0]).then((res) => {
+//   console.log(res);
+//   const record_file_id = res[0]?.record_files?.[0].record_file_id;
+//   if (!record_file_id) {
+//     return;
+//   }
+//
+//   getSummary(apiEnv.TM_USER_IDS[0], record_file_id).then(console.log);
+// });
+
+console.log("Decoded smart/fullsummary:");
+getSummary(apiEnv.TM_USER_IDS[0], '1786969809404370945').then(console.log);
+
+console.log("Decoded smart/chapters:");
+getChapters(apiEnv.TM_USER_IDS[0], '1786969809404370945').then(console.log);
