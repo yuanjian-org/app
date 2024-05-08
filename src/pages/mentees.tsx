050ca3c25bfb7d4bb6e50657b4f5dc4296aa31f4
Weihan Wang <weihanwang@users.noreply.github.com>
2024-05-08 11:54:17 -0700

    Support mentorship editing on mentee.tsx

--- src/pages/mentees.tsx
+++ src/pages/mentees.tsx
@@ -11,10 +11,20 @@ import {
   WrapItem,
   Link,
   Text,
+  Divider,
+  Button,
+  ModalHeader,
+  ModalContent,
+  ModalBody,
+  ModalFooter,
+  ModalCloseButton,
+  FormControl,
+  VStack,
+  Spacer,
 } from '@chakra-ui/react';
 import React, { useCallback, useEffect, useState } from 'react';
 import trpc, { trpcNext } from "../trpc";
-import User, { UserFilter } from 'shared/User';
+import User, { MinUser, UserFilter } from 'shared/User';
 import { formatUserName, prettifyDate, toPinyin } from 'shared/strings';
 import Loader from 'components/Loader';
 import UserFilterSelector from 'components/UserFilterSelector';
@@ -22,9 +32,12 @@ import MenteeStatusSelect from 'components/MenteeStatusSelect';
 import invariant from 'tiny-invariant';
 import { MenteeStatus } from 'shared/MenteeStatus';
 import NextLink from "next/link";
-import { ChevronRightIcon } from '@chakra-ui/icons';
+import { AddIcon, ChevronRightIcon } from '@chakra-ui/icons';
 import moment from "moment";
 import { Mentorship } from 'shared/Mentorship';
+import ModalWithBackdrop from 'components/ModalWithBackdrop';
+import UserSelector from 'components/UserSelector';
+import { MdEdit } from 'react-icons/md';
 
 const fixedFilter: UserFilter = { containsRoles: ["Mentee"] };
 
@@ -38,6 +51,8 @@ export default function Page() {
         <UserFilterSelector filter={filter} fixedFilter={fixedFilter} 
           onChange={f => setFilter(f)} />
       </Wrap>
+      
+      <Divider />
 
       {!users ? <Loader /> :
         <TableContainer>
@@ -65,7 +80,7 @@ function MenteeTable({ users, refetch }: {
       </Tr>
     </Thead>
     <Tbody>
-      {users.map((u: any) => 
+      {users.map((u: any) =>
         <MenteeRow key={u.id} user={u} refetch={refetch} />)
       }
     </Tbody>
@@ -95,11 +110,10 @@ function MenteeRow({ user: u, refetch }: {
 
   return <Tr key={u.id} _hover={{ bg: "white" }}>
     {/* 状态 */}
-    <Td><Wrap><WrapItem>
+    <Td><Wrap minWidth="110px"><WrapItem>
       <MenteeStatusSelect value={u.menteeStatus}
         size="sm" onChange={status => onChangeStatus(u, status)} />
-    </WrapItem></Wrap>
-    </Td>
+    </WrapItem></Wrap></Td>
 
     {/* 姓名 */}
     <Td><Link as={NextLink} href={`/mentees/${u.id}`}>
@@ -120,19 +134,21 @@ function MentorshipCells({ menteeId, addPinyin } : {
   menteeId : string,
   addPinyin: (names: string[]) => void,
 }) {
-  const { data } = trpcNext.mentorships.listForMentee
-    .useQuery(menteeId);
+  const { data, refetch } = trpcNext.mentorships.listForMentee.useQuery(menteeId);
   if (!data) return <Td><Loader /></Td>;
 
   // Stablize list order
   data.sort((a, b) => a.id.localeCompare(b.id));
 
-  return <LoadedMentorsCells mentorships={data} addPinyin={addPinyin} />;
+  return <LoadedMentorsCells menteeId={menteeId} mentorships={data}
+    addPinyin={addPinyin} refetch={refetch} />;
 }
 
-function LoadedMentorsCells({ mentorships, addPinyin } : {
+function LoadedMentorsCells({ menteeId, mentorships, addPinyin, refetch } : {
+  menteeId: string,
   mentorships: Mentorship[],
   addPinyin: (names: string[]) => void,
+  refetch: () => void,
 }) {
   const transcriptRes = trpcNext.useQueries(t => {
     return mentorships.map(m => t.transcripts.getMostRecentStartedAt({
@@ -146,6 +162,13 @@ function LoadedMentorsCells({ mentorships, addPinyin } : {
     return mentorships.map(m => t.users.getMentorCoach({ userId: m.mentor.id }));
   });
 
+  const refetchAll = () => {
+    refetch();
+    coachRes.map(c => c.refetch());
+  };
+
+  const [ editing, setEditing ] = useState<boolean>(false);
+
   useEffect(() => {
     const names = [
       ...mentorships.map(m => m.mentor.name),
@@ -156,25 +179,40 @@ function LoadedMentorsCells({ mentorships, addPinyin } : {
 
   return <>
     {/* 导师 */}
-    <Td>    
-      {mentorships.map(m =>
-        <Text key={m.id}>{formatUserName(m.mentor.name)}</Text>)
-      }
+    <Td>
+      {editing && <MentorshipsEditor
+        menteeId={menteeId} mentorships={mentorships}
+        coaches={coachRes.map(c => c.data === undefined ? null : c.data)}
+        onClose={() => setEditing(false)} refetch={refetchAll}
+      />}
+
+      <Link onClick={() => setEditing(true)}>
+        {mentorships.length ?
+          <VStack align="start">
+            {mentorships.map(m =>
+              <Text key={m.id}>{formatUserName(m.mentor.name)}</Text>)
+            }
+          </VStack>
+          :
+          <MdEdit />
+        }
+      </Link>
     </Td>
 
     {/* 资深导师 */}
-    <Td>
-      {coachRes.map((c, idx) =>
-        <Text key={idx}>{c.data ? formatUserName(c.data.name) : "-"}</Text>)
-      }
-    </Td>
+    <Td><Link onClick={() => setEditing(true)}><VStack align="start">
+        {coachRes.map((c, idx) => c.data ? 
+          <Text key={idx}>{formatUserName(c.data.name)}</Text> :
+          <MdEdit key={idx}/>
+        )}
+    </VStack></Link></Td>
 
     {/* 最近师生通话 */}
-    <Td>
+    <Td><VStack align="start">
       {transcriptTextAndColors.map((ttc, idx) =>
         <Text key={idx} color={ttc[1]}>{ttc[0]}</Text>
       )}
-    </Td>
+    </VStack></Td>
   </>;
 }
 
@@ -202,4 +240,105 @@ export function getDateTextAndColor(date: string | null | undefined,
     color = "grey";
   }
   return [text, color];
+}
+
+function MentorshipsEditor({ menteeId, mentorships, coaches, refetch, onClose }: {
+  menteeId: string,
+  mentorships: Mentorship[],
+  coaches: (MinUser | null)[],
+  refetch: () => void,
+  onClose: () => void,
+}) {
+  invariant(mentorships.length == coaches.length);
+  const [creating, setCreating] = useState<boolean>(false);
+
+  const saveCoach = async (mentorId: string, coachIds: string[]) => {
+    // TODO: allow removing coaches
+    if (coachIds.length == 0) return;
+    await trpc.users.setMentorCoach.mutate({
+      userId: mentorId,
+      coachId: coachIds[0],
+    });
+    refetch();
+  };
+
+  return <ModalWithBackdrop isOpen onClose={onClose}>
+    <ModalContent>
+      <ModalHeader>一对一匹配</ModalHeader>
+      <ModalCloseButton />
+      <ModalBody>
+        <TableContainer>
+          <Table>
+            <Thead>
+              <Tr>
+                <Th>导师</Th>
+                <Th>资深导师</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {mentorships.map((m: Mentorship, idx) => {
+                const coach: MinUser | null = coaches[idx];
+                return <Tr key={m.id}>
+                  <Td>{formatUserName(m.mentor.name)}</Td>
+                  <Td>
+                    <UserSelector initialValue={coach ? [coach] : []}
+                      onSelect={userIds => saveCoach(m.mentor.id, userIds)}
+                    />
+                  </Td>
+                </Tr>;
+              })}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </ModalBody>
+      <ModalFooter>
+        {creating && <MentorshipCreator menteeId={menteeId} refetch={refetch}
+          onClose={() => setCreating(false)}/>}
+        <Button variant="brand" onClick={() => setCreating(true)} 
+          leftIcon={<AddIcon />}>增加导师</Button>
+        <Spacer />
+        <Button onClick={onClose}>关闭</Button>
+      </ModalFooter>
+    </ModalContent>
+  </ModalWithBackdrop>;
+}
+
+function MentorshipCreator({ menteeId, refetch, onClose }: { 
+  menteeId: string,
+  refetch: () => void,
+  onClose: () => void,
+}) {
+  const [mentorId, setMentorId] = useState<string | null>(null);
+  const [saving, setSaving] = useState(false);
+
+  const save = async () => {
+    setSaving(true);
+    try {
+      invariant(menteeId);
+      invariant(mentorId);
+      await trpc.mentorships.create.mutate({ mentorId, menteeId });
+      refetch();
+      onClose();
+    } finally {
+      setSaving(false);
+    }
+  };
+
+  return <ModalWithBackdrop isOpen onClose={onClose}>
+    <ModalContent>
+      <ModalHeader>增加一对一导师</ModalHeader>
+      <ModalCloseButton />
+      <ModalBody>
+        <FormControl>
+          <UserSelector onSelect={
+            userIds => setMentorId(userIds.length ? userIds[0] : null)} />
+        </FormControl>
+      </ModalBody>
+      <ModalFooter>
+        <Button variant='brand'
+          isDisabled={!mentorId}
+          isLoading={saving} onClick={save}>增加</Button>
+      </ModalFooter>
+    </ModalContent>
+  </ModalWithBackdrop>;
 }
