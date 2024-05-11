46a7eab4cabcc4070e5486447418c3a4c4884f00
Yichi Zhang <83266861+UncleBu1728@users.noreply.github.com>
2024-05-11 15:18:23 -0500

    add comment for remark-gfm (#273)

--- src/components/MarkdownStyler.tsx
+++ src/components/MarkdownStyler.tsx
@@ -5,6 +5,10 @@ import styles from '../theme/MarkdownStyler.module.css';
 // This component is to correctly render Markdowns when using chakra UI
 // Chakra UI CSSReset component is turning down browser default style of elements
 // https://stackoverflow.com/a/64317290
+//
+// To enable and render GFM such as strikethrough or footnote in the future
+// Add & use remark-gfm as plugin shown in the link below
+// https://github.com/remarkjs/react-markdown?tab=readme-ov-file#use
 const MarkdownStyler = ({ content }: { content: string }) => (
   <div className={styles.markdownStyler}>
     <ReactMarkdown>{content}</ReactMarkdown>
