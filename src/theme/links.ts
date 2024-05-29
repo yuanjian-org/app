8cd24772bbebdd2482c6016b2c0dc57ac3cdb5fd
Yichi Zhang <yizhang1728@gmail.com>
2024-05-29 12:38:03 -0500

    update color and comments

--- src/theme/links.ts
+++ src/theme/links.ts
@@ -4,6 +4,7 @@ const links = {
   components: {
     Link: {
       baseStyle: {
+        // If the link color is updated, search for the hex value in the repository and update all instances manually
         color: colors.brand.c,
         _hover: {
           textDecoration: 'none',
