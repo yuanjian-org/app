01b789a4a509604d9d69fda881601eebba77fc7c
xuan7678 <xuanxuan2980@gmail.com>
2024-06-04 23:00:36 -0700

    make resource full height

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -1,5 +1,6 @@
 import {
   Text,
+  Box
 } from '@chakra-ui/react';
 import React from 'react';
 import { useUserContext } from "../UserContext";
@@ -12,6 +13,10 @@ export default fullPage(() => {
     return <Text>无权查看本页。</Text>;
   }
 
-  return <iframe src="https://f179b1fd0cd3453e9f34d95e95dc5f.super.site"
-    width="100%" height="700px" />;
+  return (
+      <Box h="calc(100vh - 60px)">
+        <iframe src="https://f179b1fd0cd3453e9f34d95e95dc5f.super.site"
+                width="100%" height="100%"/>
+      </Box>
+  );
 });
