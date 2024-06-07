c97ee536f6443502c4af6eb587318a4db3e5a14a
xuan7678 <125320632+xuan7678@users.noreply.github.com>
2024-06-06 22:21:59 -0700

    bug fix 291 - make resource full height (#309)
    * make resource full height
    
    * use variable instead of hardcode constant

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -1,10 +1,12 @@
 import {
   Text,
+  Box
 } from '@chakra-ui/react';
 import React from 'react';
 import { useUserContext } from "../UserContext";
 import { isPermitted } from 'shared/Role';
 import { fullPage } from 'AppPage';
+import { topbarHeight } from "../components/Navbars";
 
 export default fullPage(() => {
   const [user] = useUserContext();
@@ -12,6 +14,10 @@ export default fullPage(() => {
     return <Text>无权查看本页。</Text>;
   }
 
-  return <iframe src="https://f179b1fd0cd3453e9f34d95e95dc5f.super.site"
-    width="100%" height="700px" />;
+  return (
+      <Box h={`calc(100vh - ${topbarHeight})`}>
+        <iframe src="https://f179b1fd0cd3453e9f34d95e95dc5f.super.site"
+                width="100%" height="100%"/>
+      </Box>
+  );
 });
