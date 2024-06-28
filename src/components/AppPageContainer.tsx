88061f03518adfe90488ff0571b1b392cd2ac5e8
Yuqing Gong <yuqinggong.yg@gmail.com>
2024-06-27 17:17:09 -0700

    removed topbarheight import

--- src/components/AppPageContainer.tsx
+++ src/components/AppPageContainer.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren, useState } from 'react';
 
 import UserContext from "../UserContext";
 import User from '../shared/User';
-import NavBars, { sidebarBreakpoint, sidebarContentMarginTop, topbarHeight } from 'components/Navbars';
+import NavBars, { sidebarBreakpoint } from 'components/Navbars';
 import { AppPageType } from 'AppPage';
 
 export default function AppPageContainer({ pageType, user, children, ...rest }: {
@@ -17,7 +17,6 @@ export default function AppPageContainer({ pageType, user, children, ...rest }: 
     <NavBars {...rest}>
       {pageType === "full" ? children : <>
         <Box
-          marginTop={sidebarContentMarginTop}
           paddingX={{
             base: "16px",
             [sidebarBreakpoint]: "30px"
@@ -28,8 +27,8 @@ export default function AppPageContainer({ pageType, user, children, ...rest }: 
           }}
           // TODO: these hard-coded numbers are empirically measured footer heights. Replace them with constants.
           minHeight={{
-            base: `calc(100vh - ${topbarHeight} - (140px + ${footerMarginTop}))`,
-            [footerBreakpoint]: `calc(100vh - ${topbarHeight} - (95px + ${footerMarginTop}))`,
+            base: `calc(100vh  - (140px + ${footerMarginTop}))`,
+            [footerBreakpoint]: `calc(100vh -  - (95px + ${footerMarginTop}))`,
           }}
         >
           {children}
