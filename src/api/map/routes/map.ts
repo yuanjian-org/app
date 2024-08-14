467ecc18db66808436607e41dd5e0cb6c3cfd16a
Yichi Zhang <yizhang1728@gmail.com>
2024-08-14 13:43:25 -0700

    fix eslint error

--- src/api/map/routes/map.ts
+++ src/api/map/routes/map.ts
@@ -1,32 +1,28 @@
 import { procedure, router } from "../../trpc";
 import { authUser } from "../../auth";
-import db from "../../database/db";
-import { z } from "zod";
-import { noPermissionError, notFoundError } from "../../errors";
-import { isPermitted } from "shared/Role";
 
 const get = procedure
   .use(authUser("Mentee"||"Mentor"))
-  .query(async ({ input: type }) =>
+  .query(async () =>
 {
 });
 
 
 const list = procedure
   .use(authUser("Mentee"||"Mentor"))
-  .query(async ({ input: type }) =>
+  .query(async () =>
 {
 });
 
 const save = procedure
 .use(authUser("Mentee"||"Mentor"))
-.query(async ({ input: type }) =>
+.query(async () =>
 {
 });
 
 const update = procedure
 .use(authUser("Mentee"||"Mentor"))
-.query(async ({ input: type }) =>
+.query(async () =>
 {
 });
 
