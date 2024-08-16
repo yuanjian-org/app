7e4a9379af1e4a78c207a84c33035299e882cab0
Yichi Zhang <yizhang1728@gmail.com>
2024-08-16 14:38:37 -0700

    disable eslint and update I/O type

--- src/api/map/routes/map.ts
+++ src/api/map/routes/map.ts
@@ -1,12 +1,31 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable require-await */
 import { procedure, router } from "../../trpc";
 import { authUser } from "../../auth";
 import { z } from "zod";
+import { zRoles } from "shared/Role";
+
+const zLandmark = z.object({
+  定义: z.string(),
+  经度: z.string(),
+  纬度: z.string(),
+  层级: z.array(z.string()),
+  相关地标: z.array(z.string()),
+  工具箱: z.string(),
+});
+
+//type Landmark = z.TypeOf<typeof zLandmark>;
 
 const list = procedure
   .use(authUser())
-  .input(z.string())
-  .output(z.array(z.object({})))
-  .query(async ({ input: id }) =>
+  .input(z.object({
+    name: z.string(),
+    email: z.string(),
+    id: z.string(),
+    roles: zRoles,
+  }))
+  .output(z.array(zLandmark))
+  .query(async ({ input }) =>
 {
   return [];
 });
