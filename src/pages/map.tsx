72ae55b819003cad7598add339f9324e28cf3907
Yichi Zhang <yizhang1728@gmail.com>
2024-08-30 16:16:23 -0700

    update map page

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,11 +1,52 @@
+import {
+  Tab,
+  Tabs,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Flex,
+  Heading,
+  useStyleConfig,
+} from '@chakra-ui/react';
 import React from 'react';
+import { sectionSpacing } from 'theme/metrics';
 
-const Map = () => {
-    return (
-        <div>
-            Hello, I am 人才地图.
-        </div>
-    );
-};
+export default function Page() {
+  const selectedTabStyle = useStyleConfig('Tab', { variant: 'solid' });
 
-export default Map;
+  return (
+    <Flex direction="column" gap={sectionSpacing}>
+      <Tabs size = 'md'>
+         <TabList>
+          <Tab _selected={{ color: 'brand.c', borderBottom: '2px solid', borderColor: 'brand.c' }}>
+            个人成长
+          </Tab>
+          <Tab _selected={{ color: 'brand.c', borderBottom: '2px solid', borderColor: 'brand.c' }}>
+            事业发展
+          </Tab>
+          <Tab _selected={{ color: 'brand.c', borderBottom: '2px solid', borderColor: 'brand.c' }}>
+            社会责任
+          </Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <Heading as="h2" size="md" color="brand.c">
+              个人成长
+            </Heading>
+          </TabPanel>
+          <TabPanel>
+            <Heading as="h2" size="md" color="brand.c">
+              事业发展
+            </Heading>
+          </TabPanel>
+          <TabPanel>
+            <Heading as="h2" size="md" color="brand.c">
+              社会责任
+            </Heading>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Flex>
+  );
+}
