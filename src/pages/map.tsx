d6a4c1656384d3888ea9cc4b06070b9dac1b3f4d
Yichi Zhang <yizhang1728@gmail.com>
2024-09-06 09:21:48 -0700

    add trpc fetching and rendering landmarks

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -4,47 +4,69 @@ import {
   TabList,
   TabPanel,
   TabPanels,
-  Flex,
   Heading,
+  Stack,
+  Box,
+  Text,
+  Spinner,
 } from '@chakra-ui/react';
-import React from 'react';
-import { sectionSpacing } from 'theme/metrics';
+import React, { useState } from 'react';
+import { trpcNext } from "../trpc";
+import { Landmark } from 'shared/Map';
 
 export default function Page() {
+  const [tabIndex, setTabIndex] = useState(0);
 
+  const personalGrowth = trpcNext.map.list.useQuery('个人成长', { enabled: tabIndex === 0 });
+  const careerDevelopment = trpcNext.map.list.useQuery('事业发展', { enabled: tabIndex === 1 });
+  const socialResponsibility = trpcNext.map.list.useQuery('社会责任', { enabled: tabIndex === 2 });
+
+  const ChakraCards = ({ data }: { data: Landmark[] | undefined }) => {
     return (
-    <Flex direction="column" gap={sectionSpacing}>
-      <Tabs size = 'md'>
-         <TabList>
-          <Tab _selected={{ color: 'brand.c', borderBottom: '2px solid', borderColor: 'brand.c' }}>
-            个人成长
-          </Tab>
-          <Tab _selected={{ color: 'brand.c', borderBottom: '2px solid', borderColor: 'brand.c' }}>
-            事业发展
-          </Tab>
-          <Tab _selected={{ color: 'brand.c', borderBottom: '2px solid', borderColor: 'brand.c' }}>
-            社会责任
-          </Tab>
-        </TabList>
+      <Stack spacing={4}>
+        {data?.map((landmark: Landmark, index: React.Key | null | undefined) => (
+          <Box key={index}>   
+            <Heading size="md">{landmark.名称}</Heading>
+            <Text>{landmark.定义}</Text>  
+          </Box>
+        ))}
+      </Stack>
+    );
+  };
 
-        <TabPanels>
-          <TabPanel>
-            <Heading as="h2" size="md" color="brand.c">
-              个人成长
-            </Heading>
-          </TabPanel>
-          <TabPanel>
-            <Heading as="h2" size="md" color="brand.c">
-              事业发展
-            </Heading>
-          </TabPanel>
-          <TabPanel>
-            <Heading as="h2" size="md" color="brand.c">
-              社会责任
-            </Heading>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </Flex>
+  return (
+    <Tabs size="md" onChange={(index) => setTabIndex(index)}>
+      <TabList>
+        <Tab>个人成长</Tab>
+        <Tab>事业发展</Tab>
+        <Tab>社会责任</Tab>
+      </TabList>
+
+      <TabPanels>
+        <TabPanel>
+          {personalGrowth.isLoading ? (
+            <Spinner />
+          ) : (
+            <ChakraCards data={personalGrowth.data} />
+          )}
+        </TabPanel>
+
+        <TabPanel>
+          {careerDevelopment.isLoading ? (
+            <Spinner />
+          ) : (
+            <ChakraCards data={careerDevelopment.data} />
+          )}
+        </TabPanel>
+
+        <TabPanel>
+          {socialResponsibility.isLoading ? (
+            <Spinner />
+          ) : (
+            <ChakraCards data={socialResponsibility.data} />
+          )}
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   );
 }
