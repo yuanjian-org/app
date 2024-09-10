c4845253afba7a7f73a7ef2e557dce65e1945121
Yuqing Gong <yuqinggong.yg@gmail.com>
2024-09-10 11:07:44 -0700

    Toggle more when card texts exceed limit

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -9,13 +9,20 @@ import {
   Card,
   CardHeader,
   CardBody,
+  Link,
+  Flex,
+  useBreakpointValue,
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState }from 'react';
 import TabsWithUrlParam from 'components/TabsWithUrlParam';
 import { Landmark, Latitude  } from 'shared/Map';
 import { trpcNext } from '../../trpc';
 import { componentSpacing } from 'theme/metrics';
 import Loader from 'components/Loader';
+import { sidebarBreakpoint } from 'components/Navbars';
+
+const desktopTextLimit = 120;
+const mobileTextLimit = 60;
 
 export default function Page() {
 
@@ -46,7 +53,7 @@ export default function Page() {
 
 const LandmarkTabPanel = ({ latitude }: { latitude: Latitude }) => {
   const { data, isLoading } = trpcNext.map.list.useQuery(latitude);
-
+  
   return (
     <>
       {isLoading ? 
@@ -63,14 +70,30 @@ const LandmarkTabPanel = ({ latitude }: { latitude: Latitude }) => {
 };
 
 const LandmarkCard = ({ landmark }: { landmark: Landmark })  => {
+  // Ensure maxChar is never undefined by giving a default in useBreakpointValue
+  const maxChar = useBreakpointValue({ base: mobileTextLimit, 
+    [sidebarBreakpoint]: desktopTextLimit }) || desktopTextLimit;
+  const [isExpanded, setIsExpanded] = useState(false);
+  const handleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+  const displayText = isExpanded || landmark.定义.length <= maxChar
+    ? landmark.定义
+    : `${landmark.定义.substring(0, maxChar)}...`;
+
   return (
     <Card>
       <CardHeader>
         <Heading size="md">{landmark.名称}</Heading>
       </CardHeader>
       <CardBody>
-        <Text>{landmark.定义}</Text>
+        <Flex direction="column">
+          <Text>{displayText}</Text>
+          {landmark.定义.length > maxChar && 
+          <Link alignSelf="end" onClick={handleExpand}> 
+          {isExpanded ? '收起' : '更多'}</Link>}
+        </Flex>
       </CardBody>
-    </Card>
+    </Card> 
   );
 };
