7e35e66beb47e0156deb062a0ca271a0635a9bac
Weihan Wang <weihanwang@users.noreply.github.com>
2024-09-21 23:26:00 -0700

    Set NEXTAUTH_URL for non-vercel production

--- src/shared/getBaseUrl.ts
+++ src/shared/getBaseUrl.ts
@@ -4,8 +4,9 @@ export default function getBaseUrl() {
   if (typeof window !== 'undefined') return '';
   // vercel.com
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-  // non-vercel.com production environemnt (e.g. Docker), set in .env file
-  if (process.env.PUBLIC_URL) return process.env.PUBLIC_URL;
+  // non-vercel.com production environemnt (e.g. Docker). We piggyback on
+  // nextauth's configuration.
+  if (process.env.NEXTAUTH_URL) return process.env.NEXTAUTH_URL;
   // fall back to localhost
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
