1b0b787d3d4eb03ee4380ee91cde95a1ceb8f483
Weihan Wang <weihanwang@users.noreply.github.com>
2024-09-21 20:50:00 -0700

    Remove https from PUBLIC_URL for testing

--- src/trpc.ts
+++ src/trpc.ts
@@ -10,7 +10,7 @@ function getBaseUrl() {
   // vercel.com
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   // non-vercel.com production environemnt (e.g. Docker), set in .env file
-  if (process.env.PUBLIC_URL) return `https://${process.env.PUBLIC_URL}`;
+  if (process.env.PUBLIC_URL) return process.env.PUBLIC_URL;
   // fall back to localhost
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
