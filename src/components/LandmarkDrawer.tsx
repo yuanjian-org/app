18677c566eca5c20558f986438069033e9fa8493
Yuqing Gong <yuqinggong.yg@gmail.com>
2024-09-23 14:36:55 -0700

    add four levels for each landmark

--- src/components/LandmarkDrawer.tsx
+++ src/components/LandmarkDrawer.tsx
@@ -5,19 +5,41 @@ import {
     DrawerOverlay, 
     DrawerContent, 
     DrawerCloseButton,
+    Radio,
+    RadioGroup,
+    Stack,
+    Text,
+    Button,
   } from '@chakra-ui/react';
 import { Landmark } from 'shared/Map';
+import React, { useState } from 'react';
+import { componentSpacing } from 'theme/metrics';
 
 export default function LandmarkDrawer ({ onClose, landmark }: { 
     onClose: () => void; 
     landmark: Landmark
   }) {      
-    return <Drawer isOpen onClose={onClose}>
+    const [value, setValue] = useState('');
+
+    return <Drawer size="lg" isOpen onClose={onClose}>
       <DrawerOverlay />
       <DrawerContent>
         <DrawerCloseButton /> 
         <DrawerHeader>{landmark.名称}</DrawerHeader>
-        <DrawerBody>{landmark.定义}</DrawerBody>
+        <DrawerBody display="flex" flexDirection="column" gap={componentSpacing}>
+          {landmark.定义}
+          <Text>你认为你的{landmark.名称}处于以下哪个阶段？（单选）</Text>
+          <RadioGroup onChange={setValue} value={value}>
+            <Stack direction="column">
+              {landmark.层级.map((level, index) => (
+                <Radio key={index} value={String(index)} alignItems="flex-start">
+                  {level}
+                </Radio>
+              ))}
+            </Stack>
+          </RadioGroup>
+          <Button width="20%" alignSelf="center">提交</Button>     
+        </DrawerBody>
       </DrawerContent>
     </Drawer>;
   };
