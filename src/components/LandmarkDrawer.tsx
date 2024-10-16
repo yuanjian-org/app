a5036edb872d2bf9b7d5f352cce480ddc97c3317
ada-only-one-ada <122472773+ada-only-one-ada@users.noreply.github.com>
2024-10-16 15:25:06 -0700

    Load (#415)

--- src/components/LandmarkDrawer.tsx
+++ src/components/LandmarkDrawer.tsx
@@ -36,6 +36,7 @@ import ModalWithBackdrop from './ModalWithBackdrop';
 import { formatUserName } from 'shared/strings';
 import invariant from "tiny-invariant";
 import { compareDate } from 'shared/strings';
+import Loader from './Loader';
 
 const desktopTextLimit = 60;
 const mobileTextLimit = 20;
@@ -138,7 +139,8 @@ function LandmarkAssessmentHistory({ landmark } : {
         <Th>详情</Th>
       </Tr>
       <Tbody>
-        {assessments?.sort((a, b) => compareDate(a.createdAt, b.createdAt))
+        {!assessments ?  <Loader /> :
+        assessments.sort((a, b) => compareDate(a.createdAt, b.createdAt))
         .map((assessment, index) => (
           <Tr 
             key={index} 
