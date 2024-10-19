74170aa7bdeb8e429145ba6a7c6e67edcca2f8d6
Yuqing Gong <yuqinggong.yg@gmail.com>
2024-10-18 23:32:09 -0700

    Add comments

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -292,6 +292,8 @@ function DropdownMenu({ title, icon, menuItems, onClose } : {
             // Only sets the link it is a url 
             {...isUrl && { as: NextLink,  href: item.action } }
             onClick={() => {
+              // (item as { action: Function }).action() 
+              // the above code will cause run time error if action is a URL.
               if (typeof item.action === 'function') item.action();
               onClose();
             }}>
