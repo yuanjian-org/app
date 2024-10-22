eca62b079b822fee803f4eab77a79960d588bc55
Weihan Wang <weihanwang@users.noreply.github.com>
2024-10-21 17:07:39 -0700

    Revert "sort interviews by sources then names (#391)" (#423)
    This reverts commit daa459e015add58de7006eab60ddeea100224ce5.

--- src/pages/mentees.tsx
+++ src/pages/mentees.tsx
@@ -108,15 +108,16 @@ function MenteeTable({ users, refetch }: {
     </Thead>
     <Tbody>
       {users.sort((a: User, b: User) => {
-        const comp = (menteeToYear.get(b.id) || "")
-          .localeCompare(menteeToYear.get(a.id) || "");
-        return comp !== 0 ? comp 
-          : 
-          // Need to convert it to pinyin, otherwise the result 
-          // will not be correct if compare Chinese directly. Ref:
-          // https://www.leevii.com/2023/04/about-the-inaccurate-chinese-sorting-of-localecompare.html
-          (toPinyin(formatUserName(a.name, 'formal')))
-            .localeCompare(toPinyin(formatUserName(b.name, 'formal')));
+        const yearA = menteeToYear.get(a.id) || "";
+        const yearB = menteeToYear.get(b.id) || "";
+
+        if (yearA === yearB) {
+          const nameA = formatUserName(a.name, 'formal');
+          const nameB = formatUserName(b.name, 'formal');
+          return nameA.localeCompare(nameB);
+        }
+
+        return yearB.localeCompare(yearA);
       }).map((u: User) => <MenteeRow key={u.id} user={u} refetch={refetch} 
           updateMenteeYear={updateMenteeYear} />)
       }
