e1440a04e969350b566639654b5e85164dc70fd3
Weihan Wang <weihanwang@users.noreply.github.com>
2024-10-21 17:07:05 -0700

    Revert "sort interviews by sources then names (#391)"
    This reverts commit daa459e015add58de7006eab60ddeea100224ce5.

--- src/pages/interviews.tsx
+++ src/pages/interviews.tsx
@@ -31,7 +31,7 @@ import {
   UnorderedList,
   ListItem,
 } from '@chakra-ui/react';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { trpcNext } from "../trpc";
 import ModalWithBackdrop from 'components/ModalWithBackdrop';
 import trpc from 'trpc';
@@ -118,11 +118,6 @@ function Applicants({ type, applicants, interviews, refetchInterviews,
   refetchInterviews: () => void,
   refetchApplicants: () => void,
 }) {
-  // A map from applicants' user ids to sources.
-  const [sources, setSources] = useState<{ [id: string]: string }>({});
-  const updateSource = (id: string, source: string) => {
-    setSources(prev => ({ ...prev, [id]: source }));
-  };
   return <TableContainer>
     <Text marginBottom={sectionSpacing} color="grey" fontSize="sm">
       点击候选人以编辑面试官和面试讨论组：
@@ -138,19 +133,10 @@ function Applicants({ type, applicants, interviews, refetchInterviews,
         </Tr>
       </Thead>
       <Tbody>
-        {applicants
-          .sort((a1, a2) => {
-            const comp = toPinyin(sources[a1.id] || '')
-                .localeCompare(toPinyin(sources[a2.id] || ''));
-            return comp !== 0 ? comp : 
-              toPinyin(formatUserName(a1.name, 'formal'))
-                .localeCompare(toPinyin(formatUserName(a2.name, 'formal')));
-          })
-          .map(a => 
+        {applicants.map(a => 
           <Applicant key={a.id} type={type} applicant={a}
             interviews={interviews} refetchInterviews={refetchInterviews}
-            refetchApplicants={refetchApplicants} 
-            updateSource={source => updateSource(a.id, source)}
+            refetchApplicants={refetchApplicants}
           />)
         }
       </Tbody>
@@ -167,22 +153,17 @@ function Applicants({ type, applicants, interviews, refetchInterviews,
 }
 
 function Applicant({ type, applicant, interviews, refetchInterviews,
-  refetchApplicants, updateSource
+  refetchApplicants
  } : {
   type: InterviewType,
   applicant: User,
   interviews: Interview[],
   refetchInterviews: () => void,
   refetchApplicants: () => void,
-  updateSource:(source: string) => void,
 }) {
   // TODO: it's duplicative to fetch the applicant again
   const { data } = trpcNext.users.getApplicant.useQuery({ userId: applicant.id, type });
   const source = (data?.application as Record<string, any> | null)?.[menteeSourceField];
-
-  useEffect(() => {
-    updateSource(source);
-  }, [source, updateSource]);
 
   const matches = interviews.filter(i => i.interviewee.id == applicant.id);
   if (matches.length > 1) {
