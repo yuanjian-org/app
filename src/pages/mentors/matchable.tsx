e8b7d599fece1dc305e5c93e4f0dfd31614342b0
Weihan Wang <weihanwang@users.noreply.github.com>
2024-11-17 22:36:35 -0800

    Improve mentor profile pages (#499)

--- src/pages/mentors/matchable.tsx
+++ src/pages/mentors/matchable.tsx
@@ -2,6 +2,6 @@ import { widePage } from "AppPage";
 import { MentorPage } from ".";
 
 export default widePage(
-  () => <MentorPage matchableOnly title="一对一导师" />,
+  () => <MentorPage matchable title="一对一导师" />,
   "一对一导师"
 );
