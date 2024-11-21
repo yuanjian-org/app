25035220af1bfc2df932070b25239c72fe5dca1e
Weihan Wang <weihanwang@users.noreply.github.com>
2024-11-20 22:03:50 -0800

    Break out mentors page menu (#504)

--- src/pages/mentors/matchable.tsx
+++ src/pages/mentors/matchable.tsx
@@ -2,6 +2,6 @@ import { widePage } from "AppPage";
 import { MentorPage } from ".";
 
 export default widePage(
-  () => <MentorPage matchable title="一对一导师" />,
-  "一对一导师"
+  () => <MentorPage matchable title="选择一对一导师" />,
+  "选择一对一导师"
 );
