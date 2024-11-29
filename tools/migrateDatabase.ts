88600e17596c85abbf85040ec91feedba7bbaf96
Weihan Wang <weihanwang@users.noreply.github.com>
2024-11-29 09:40:19 -0800

    Minor UI and script adjustments

--- tools/migrateDatabase.ts
+++ tools/migrateDatabase.ts
@@ -1,8 +1,15 @@
 import sequelize from "../src/api/database/sequelize";
 import { migrateDatabase } from "../src/api/routes/migration";
+import { adapter } from "../src/pages/api/auth/[...nextauth]";
 
 async function sync() {
+  // Register the next-auth adapter so sequelize.sync() will create tables for
+  // next-auth.
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const _ = adapter;
+
   await migrateDatabase();
+
   // This make sure the process doesn't hang waiting for connection closure.
   await sequelize.close();
 }
