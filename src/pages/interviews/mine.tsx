dbb464e80447896395aa654aaeb64cf40d9467c6
Weihan Wang <weihanwang@users.noreply.github.com>
2024-11-29 09:40:19 -0800

    Minor UI and script adjustments

--- src/pages/interviews/mine.tsx
+++ src/pages/interviews/mine.tsx
@@ -2,27 +2,23 @@ import {
   Link,
   HStack,
   Text,
+  Box,
 } from '@chakra-ui/react';
 import React from 'react';
 import { trpcNext } from "../../trpc";
 import Interviews from 'components/Interviews';
-import { widePage } from 'AppPage';
 import PageBreadcrumb from 'components/PageBreadcrumb';
 import NextLink from 'next/link';
 import { sectionSpacing } from 'theme/metrics';
 import { IoChatbubblesOutline } from "react-icons/io5";
 
-export default widePage(() => {
+export default function Page () {
   const { data: interviews } = trpcNext.interviews.listMine.useQuery();
   const { data: calibrations } = trpcNext.calibrations.listMine.useQuery();
 
   return <>
-    <PageBreadcrumb current='我的面试' />
-    <Interviews interviews={interviews} forCalibration={false}
-      hideTotalCount />
-
-    {calibrations && calibrations.length && <>
-      <PageBreadcrumb current='面试讨论组' my={sectionSpacing} />
+    {calibrations && calibrations.length && <Box mb={sectionSpacing}>
+      <PageBreadcrumb current='面试讨论组' />
       {calibrations.map(c => <Link
         key={c.id} 
         as={NextLink}
@@ -33,6 +29,12 @@ export default widePage(() => {
           <Text>{c.name}</Text>
         </HStack>
       </Link>)}
-    </>}
+    </Box>}
+
+    <PageBreadcrumb current='我的面试' />
+    <Interviews interviews={interviews} forCalibration={false}
+      hideTotalCount />
   </>;
-}, "我的面试");
+}
+
+Page.title = "我的面试";
