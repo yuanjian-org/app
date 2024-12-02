8d591e8d8bdddbe300d8e66a77b399cf3b6eeaf8
Weihan Wang <weihanwang@users.noreply.github.com>
2024-12-01 23:45:31 -0800

    add wechat login demo (#559)
    Co-authored-by: huxingcan <huxingcan@zingfront.com>

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,13 +1,16 @@
-import { Button, InputGroup, InputLeftElement, Input, Heading } from '@chakra-ui/react';
+import { Button, InputGroup, InputLeftElement, Input, Heading, Text, VStack, Link, HStack } from '@chakra-ui/react';
 import { EmailIcon } from '@chakra-ui/icons';
 import { signIn } from "next-auth/react";
 import { useEffect, useState } from "react";
 import z from "zod";
 import { useRouter } from 'next/router';
-import { parseQueryString } from "shared/strings";
+import { parseQueryString, parseQueryStringOrUnknown } from "shared/strings";
 import { toast } from 'react-toastify';
 import trpc from 'trpc';
 import { RoleProfiles } from 'shared/Role';
+import Image from 'next/image';
+import WeChatQRLogin from 'components/WeChatQRLogin';
+import { componentSpacing, sectionSpacing } from 'theme/metrics';
 
 export const localStorageKeyForLoginCallbackUrl = "loginCallbackUrl";
 export const localStorageKeyForLoginEmail = "loginEmail";
@@ -20,6 +23,7 @@ export const callbackUrlKey = "callbackUrl";
  */
 export default function Page() {
   const router = useRouter();
+  const showWeChat = parseQueryStringOrUnknown(useRouter(), 'demo') === '1';
 
   // Use the last login email
   const [email, setEmail] = useState<string>("");
@@ -41,10 +45,10 @@ export default function Page() {
       // See https://next-auth.js.org/configuration/pages#error-page
       console.error(`Unkonwn error on /auth/verify: ${err}`);
       toast.error(`糟糕，系统错误，请联系管理员：${err}`);
-    }  
+    }
   }, [router]);
 
-  const submit = async () => {
+  const submitEmail = async () => {
     const callbackUrl = parseQueryString(router, callbackUrlKey) ?? "/";
     setIsLoading(true);
     try {
@@ -80,25 +84,65 @@ export default function Page() {
 
   const isValidEmail = () => z.string().email().safeParse(email).success;
 
+  const isWechatBrowser = /MicroMessenger/i.test(navigator.userAgent);
+
   return <>
     <Heading size="md" marginBottom={10}>社会导师服务平台</Heading>
 
+    {/* 微信扫码登录 */}
+    {showWeChat && !isWechatBrowser && <VStack spacing={componentSpacing}>
+      <Text>微信扫码登录</Text>
+      <WeChatQRLogin />
+    </VStack>}
+
+    {/* 微信服务号登录 */}
+    {showWeChat && isWechatBrowser && <Link
+      onClick={() => signIn('wechat')}
+    >
+      <HStack mb={sectionSpacing}>
+        <Image src="/img/wechat.svg" alt="WeChat" width={24} height={24} />
+        <Text>微信登录</Text>
+      </HStack>
+    </Link>}
+
+    {/* 微信扫码登陆(点击版) */}
+    {/* <Button
+      width="full"
+      mt={2}
+      leftIcon={<Image src="/img/wechat.svg" alt="WeChat" width={24} height={24} />}
+      onClick={() => signIn('wechat-qr')}
+      bg="#07C160"
+      color="white"
+      _hover={{ bg: "#06AE56" }}
+    >
+      微信扫码登陆(点击版)
+    </Button> */}
+
+    {/* Email login */}
     <InputGroup>
       <InputLeftElement pointerEvents='none'>
         <EmailIcon color='gray.400' />
       </InputLeftElement>
-
-      {/* `name="email"` to express intent for password management tools */}
-      <Input type="email" name="email" minWidth={80} placeholder="请输入邮箱"
-        autoFocus value={email} onChange={(ev) => setEmail(ev.target.value)}
+      <Input
+        type="email"
+        name="email"
+        minWidth={80}
+        placeholder="请输入邮箱"
+        autoFocus
+        value={email}
+        onChange={(ev) => setEmail(ev.target.value)}
         onKeyDown={async ev => {
-          if (ev.key == "Enter" && isValidEmail()) await submit(); 
+          if (ev.key == "Enter" && isValidEmail()) await submitEmail();
         }}
       />
     </InputGroup>
 
-    <Button variant="brand" width="full" isDisabled={!isValidEmail()}
-      isLoading={isLoading} onClick={submit}
+    <Button
+      variant="brand"
+      width="full"
+      isDisabled={!isValidEmail()}
+      isLoading={isLoading}
+      onClick={submitEmail}
     >登录 / 注册</Button>
   </>;
 }
