ac4437663d213d3411f285be85204db96e4a77c1
Weihan Wang <weihanwang@users.noreply.github.com>
2025-01-16 18:45:50 -0800

    Add StudyGadget

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,11 @@ import { Grid, VStack } from '@chakra-ui/react';
 import PageBreadcrumb from 'components/PageBreadcrumb';
 import GroupsGadget from 'components/gadgets/GroupsGadget';
 import NewsGadget from 'components/gadgets/NewsGadget';
+import StudyGadget from 'components/gadgets/StudyGadget';
+import { PropsWithChildren } from 'react';
+import { isPermitted } from 'shared/Role';
 import { breakpoint, sectionSpacing } from 'theme/metrics';
+import { useMyRoles } from 'useMe';
 
 const title = "个人空间";
 
@@ -14,14 +18,23 @@ export default function Page() {
       templateColumns={{ base: "1fr", [breakpoint]: "1fr 1fr" }} 
       gap={sectionSpacing}
     >
-      <VStack w="full" align="stretch">
+      <Column>
         <GroupsGadget />
-      </VStack>
-      <VStack w="full" align="stretch">
+      </Column>
+
+      <Column>
         <NewsGadget />
-      </VStack>
+
+        {isPermitted(useMyRoles(), "Volunteer") && <StudyGadget />}
+      </Column>
     </Grid>
   </>);
+}
+
+function Column({ children }: PropsWithChildren) {
+  return <VStack w="full" align="stretch" spacing={sectionSpacing}>
+    {children}
+  </VStack>;
 }
 
 Page.title = title;
