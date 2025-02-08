fe311bf77f5cb8e94883ec56350d0af8a2b038ef
Weihan Wang <weihanwang@users.noreply.github.com>
2025-02-07 19:46:33 -0800

    Support PoC and Source fields in match output (#645)

--- src/shared/MatchSolution.ts
+++ src/shared/MatchSolution.ts
@@ -3,6 +3,9 @@ import { zMinUser } from "./User";
 
 export const zMatchSolution = z.array(z.object({
   mentee: zMinUser,
+  pointOfContact: zMinUser.nullable(),
+  // 机构来源
+  source: z.string().nullable(),
   // Mentors in the solution that the mentee preferred
   preferredMentors: z.array(zMinUser),
   // Mentors not in the solution that the mentee preferred
