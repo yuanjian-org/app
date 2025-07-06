864fca70254f6ddc65ae85f06435f8c07b671b63
Weihan Wang <weihanwang@users.noreply.github.com>
2025-07-06 15:59:41 +0800

    Reformat eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,67 +8,71 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 export default defineConfig([
-    globalIgnores([
-        "src/api/database/sequelize-adapter-src", 
-        "**/next.config.js"
-    ]),
-        {
-        extends: compat.extends("next/core-web-vitals", "plugin:@typescript-eslint/recommended"),
+  globalIgnores([
+    "src/api/database/sequelize-adapter-src",
+    "**/next.config.js",
+  ]),
 
-        languageOptions: {
-            parser: tsParser,
-            ecmaVersion: 5,
-            sourceType: "script",
+  {
+    extends: compat.extends(
+      "next/core-web-vitals",
+      "plugin:@typescript-eslint/recommended",
+    ),
 
-            parserOptions: {
-                project: ["./tsconfig.json"],
-            },
-        },
+    rules: {
+      "object-curly-spacing": ["error", "always"],
+      semi: ["error", "always"],
 
-        rules: {
-            "object-curly-spacing": ["error", "always"],
-            semi: ["error", "always"],
+      "import/no-restricted-paths": ["error", {
+        zones: [{
+          target: "./src/api",
+          from: ["./src"],
+          except: ["./api", "./shared", "./pages/api"],
+          message: "Files in `src/api` must not refer to files outside of `src/api`, `src/shared` or `src/pages/api`. See README.md",
+        }, {
+          target: "./src/shared",
+          from: ["./src"],
+          except: ["./shared"],
+          message: "Files in `src/shared` must not refer to files outside of `src/shared`. See README.md",
+        }, {
+          target: [
+            "./src/!(api|pages)/**/*",
+            "./src/pages/!(api)/**/*",
+            "./src/*.[t]*",
+            "./src/pages/*.[t]*",
+          ],
 
-            "import/no-restricted-paths": ["error", {
-                zones: [{
-                    target: "./src/api",
-                    from: ["./src"],
-                    except: ["./api", "./shared", "./pages/api"],
-                    message: "Files in `src/api` must not refer to files outside of `src/api`, `src/shared` or `src/pages/api`. See README.md",
-                }, {
-                    target: "./src/shared",
-                    from: ["./src"],
-                    except: ["./shared"],
-                    message: "Files in `src/shared` must not refer to files outside of `src/shared`. See README.md",
-                }, {
-                    target: [
-                        "./src/!(api|pages)/**/*",
-                        "./src/pages/!(api)/**/*",
-                        "./src/*.[t]*",
-                        "./src/pages/*.[t]*",
-                    ],
+          from: ["./src/api/"],
+          except: ["./apiRouter.ts"],
+          message: "Files outside of `src/api` must not refer to files in `src/api`. See README.md",
+        }],
+      }],
 
-                    from: ["./src/api/"],
-                    except: ["./apiRouter.ts"],
-                    message: "Files outside of `src/api` must not refer to files in `src/api`. See README.md",
-                }],
-            }],
-
-            "import/no-absolute-path": "error",
-            "require-await": "error",
-            "@typescript-eslint/no-floating-promises": "error",
-            "@typescript-eslint/await-thenable": "error",
-            
-            // TODO: Other @typescript-eslint rules to be reset to default
-            "@typescript-eslint/ban-ts-comment": "off",
-            "@typescript-eslint/ban-types": "off",
-            "@typescript-eslint/no-explicit-any": "off",
-        },
+      "import/no-absolute-path": "error",
+      "require-await": "error",
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/await-thenable": "error",
+    
+      // TODO: Other @typescript-eslint rules to be reset to default
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/ban-types": "off",
+      "@typescript-eslint/no-explicit-any": "off",
     },
+  },
+
+  {
+    files: ["src/**/*.ts", "src/**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+    },
+  },
 ]);
--- src/shared/strings.ts
+++ src/shared/strings.ts
@@ -127,9 +127,9 @@ export function shaChecksum(obj: Record<string, any>): string {
 export function hash(str: string): number {
   let hash = 0;
   for (let i = 0; i < str.length; i++) {
-      const char = str.charCodeAt(i);
-      hash = (hash << 5) - hash + char;
-      hash = hash & hash; // Convert to 32-bit integer
+    const char = str.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash = hash & hash; // Convert to 32-bit integer
   }
   return Math.abs(hash);
 }
