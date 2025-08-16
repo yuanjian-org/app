d06da2fe94fdbea2d30b18a30fcd5c2a1ecd7f61
Weihan Wang <weihanwang@users.noreply.github.com>
2025-08-16 13:38:36 -0700

    Enforce exams on /interviews/mine page

--- src/pages/interviews/mine.tsx
+++ src/pages/interviews/mine.tsx
@@ -1,40 +1,50 @@
 import { Link, HStack, Text } from "@chakra-ui/react";
-import React from "react";
 import { trpcNext } from "../../trpc";
 import Interviews from "components/Interviews";
 import PageBreadcrumb from "components/PageBreadcrumb";
 import NextLink from "next/link";
 import { sectionSpacing } from "theme/metrics";
 import { IoChatbubblesOutline } from "react-icons/io5";
+import { ExamsRequired, useExamsRequired } from "components/ExamsRequired";
 
 export default function Page() {
   const { data: interviews } = trpcNext.interviews.listMine.useQuery();
   const { data: calibrations } = trpcNext.calibrations.listMine.useQuery();
+  const { commsExamRequired, interviewExamRequired } = useExamsRequired();
 
-  return (
-    <>
-      <PageBreadcrumb current="我的面试" />
-      <Interviews
-        interviews={interviews}
-        forCalibration={false}
-        hideTotalCount
+  if (commsExamRequired || interviewExamRequired) {
+    return (
+      <ExamsRequired
+        interviewExamRequired={interviewExamRequired ?? false}
+        commsExamRequired={commsExamRequired ?? false}
       />
+    );
+  } else {
+    return (
+      <>
+        <PageBreadcrumb current="我的面试" />
+        <Interviews
+          interviews={interviews}
+          forCalibration={false}
+          hideTotalCount
+        />
 
-      {calibrations && calibrations.length > 0 && (
-        <>
-          <PageBreadcrumb current="面试讨论组" mt={sectionSpacing * 2} />
-          {calibrations.map((c) => (
-            <Link key={c.id} as={NextLink} href={`/calibrations/${c.id}`}>
-              <HStack>
-                <IoChatbubblesOutline />
-                <Text>{c.name}</Text>
-              </HStack>
-            </Link>
-          ))}
-        </>
-      )}
-    </>
-  );
+        {calibrations && calibrations.length > 0 && (
+          <>
+            <PageBreadcrumb current="面试讨论组" mt={sectionSpacing * 2} />
+            {calibrations.map((c) => (
+              <Link key={c.id} as={NextLink} href={`/calibrations/${c.id}`}>
+                <HStack>
+                  <IoChatbubblesOutline />
+                  <Text>{c.name}</Text>
+                </HStack>
+              </Link>
+            ))}
+          </>
+        )}
+      </>
+    );
+  }
 }
 
 Page.title = "我的面试";
