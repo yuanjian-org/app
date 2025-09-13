27bc7574357ce99f4d1de959524d68397a5009c1
Weihan Wang <weihanwang@users.noreply.github.com>
2025-09-13 09:17:47 -0700

    Add Fundebug support

--- src/components/AppPageContainer.tsx
+++ src/components/AppPageContainer.tsx
@@ -1,12 +1,14 @@
 import { Spacer, VStack } from "@chakra-ui/react";
 import Footer from "components/Footer";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect } from "react";
 
 import NavBars from "components/Navbars";
 import { breakpoint, pageMarginTop } from "theme/metrics";
 import { AppPageType } from "AppPage";
 import { pageMarginX } from "theme/metrics";
 import PostLoginModels from "./PostLoginModels";
+import { useMyId } from "useMe";
+import { initFundebug } from "fundebug";
 
 export default function AppPageContainer({
   pageType,
@@ -14,6 +16,13 @@ export default function AppPageContainer({
 }: {
   pageType?: AppPageType;
 } & PropsWithChildren) {
+  const myId = useMyId();
+
+  useEffect(() => {
+    // Note that we don't reset the user id when the user logs out.
+    if (myId) initFundebug(myId);
+  }, [myId]);
+
   return (
     <>
       <NavBars>
