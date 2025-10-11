731a3300dfcbf1fdf60dde7136e82139dba13fa6
Weihan Wang <weihanwang@users.noreply.github.com>
2025-10-11 15:55:27 -0700

    Purge old data weekly (#755)

--- src/api/routes/migration.ts
+++ src/api/routes/migration.ts
@@ -14,28 +14,6 @@ export async function migrateDatabase() {
   await migrateSchema();
   await sequelize.sync({ alter: { drop: false } });
   await migrateData();
-  await purgeOldData();
-}
-
-async function purgeOldData() {
-  console.log("Purge old data...");
-  await sequelize.query(`
-    DELETE FROM "EventLogs"
-    WHERE "updatedAt" < NOW() - INTERVAL '1 year';
-  `);
-
-  await sequelize.query(`
-    DELETE FROM "InterviewFeedbackUpdateAttempts"
-    WHERE "updatedAt" < NOW() - INTERVAL '30 days';
-  `);
-
-  // MeetingHistories table is used to retrieve meeting summaries from Tencent
-  // Meeting API. Tencent retains meeting records only for 30 days. Keep the
-  // history here a bit longer for debugging purposes.
-  await sequelize.query(`
-    DELETE FROM "MeetingHistories"
-    WHERE "updatedAt" < NOW() - INTERVAL '60 days';
-  `);
 }
 
 async function migrateSchema() {
