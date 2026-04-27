f9eb7bb5eb91c0217d8d374c8af8ca7db1c39a01
Weihan Wang <weihanwang@users.noreply.github.com>
2026-04-26 17:13:51 -0700

    Update UstcLandingPage.tsx

--- src/components/UstcLandingPage.tsx
+++ src/components/UstcLandingPage.tsx
@@ -72,11 +72,10 @@ export default function UstcLandingPage() {
         mt="70px"
       >
         <VStack spacing={6} align="start" mt={10}>
-          <Heading size="lg">
-            欢迎来到“远图”：中国科学技术大学社会导师网络
-          </Heading>
+          <Heading size="lg">欢迎来到中国科学技术大学社会导师网络</Heading>
           <Text>
-            社会导师项目由中科大校友发起并提供，旨在为中科大在校学生提供一对一的陪伴与指导。通过过来人的经验分享与帮助，助力同学们更好地规划学业与职业发展，顺利步入社会。
+            社会导师项目由中科大校友发起并提供，旨在为中科大在校学生提供一对一的陪伴与指导。通过过来人的经验分享与帮助，助力同学们更好地规划学业与职业发展，顺利步入社会。“远图”
+            是支持这一项目的服务平台。
           </Text>
           <Button
             size="lg"
