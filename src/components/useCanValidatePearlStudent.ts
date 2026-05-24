e3cce375d3a8b4edb8e32b7c418351fe0db9d7eb
Weihan Wang <weihanwang@users.noreply.github.com>
2026-05-23 20:30:15 -0700

    Refactoring

--- src/components/useCanValidatePearlStudent.ts
+++ src/components/useCanValidatePearlStudent.ts
@@ -1,11 +1,7 @@
 import useWhiteLabel from "components/useWhiteLabel";
 import Role from "../shared/Role";
 
-/**
- * A user can validate themselves as a pearl student only if the white label is
- * "xhef" and they have no roles.
- */
-export function useCanValidatePearlStudent(myRoles: Role[]) {
+export function useCanValidatePearlStudent(roles: Role[]) {
   const whiteLabel = useWhiteLabel();
-  return whiteLabel === "xhef" && myRoles.length === 0;
+  return whiteLabel === "xhef" && roles.length === 0;
 }
