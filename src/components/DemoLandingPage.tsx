f6a9225de6caf5eaf816662da61db30b57f7254f
Weihan Wang <weihanwang@users.noreply.github.com>
2026-05-31 16:12:04 -0700

    Update DemoLandingPage.tsx

--- src/components/DemoLandingPage.tsx
+++ src/components/DemoLandingPage.tsx
@@ -74,11 +74,11 @@ export default function DemoLandingPage() {
         <VStack spacing={6} align="start" mt={10}>
           <Heading size="lg">欢迎来到远图平台体验版</Heading>
           <Text>
-            这是一个用于演示和测试目的的独立环境。在这个平台上，你可以体验远图社会导师服务平台的功能。
+            这是一个用于演示和测试目的的独立环境。在这个平台上，您可以体验远图社会导师服务平台的功能。
             请注意，这里的数据仅用于演示，不与正式环境同步，并且每天会重置一次。
           </Text>
           <Text>
-            如果你想访问正式的远图平台，请前往：
+            如果您想访问正式的远图平台，请前往：
             <Link
               as={NextLink}
               href="https://yuantuapp.com"
