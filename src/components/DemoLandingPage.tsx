d21277921e0772ccf285c479ef7b53dfe1b7d635
Weihan Wang <weihanwang@users.noreply.github.com>
2026-05-31 16:16:34 -0700

    Update DemoLandingPage.tsx

--- src/components/DemoLandingPage.tsx
+++ src/components/DemoLandingPage.tsx
@@ -78,15 +78,21 @@ export default function DemoLandingPage() {
             请注意，这里的数据仅用于演示，不与正式环境同步，并且每天会重置一次。
           </Text>
           <Text>
-            如果您想访问正式的远图平台，请前往：
-            <Link
-              as={NextLink}
-              href="https://yuantuapp.com"
-              isExternal
-              color="blue.500"
-            >
+            如果您想访问正式的远图平台，请前往{" "}
+            <Link href="https://yuantuapp.com" isExternal>
               yuantuapp.com
             </Link>
+            。
+          </Text>
+          <Text>
+            如果您想获取体验版的登录账号，
+            <Link
+              href="https://work.weixin.qq.com/kfid/kfcd32727f0d352531e"
+              isExternal
+            >
+              请联系我们
+            </Link>
+            。
           </Text>
           <Button
             size="lg"
