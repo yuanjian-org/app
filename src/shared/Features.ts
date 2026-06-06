35489c703973d4edc67dbeaf45f59455574b7ba3
WH <weihanwang@users.noreply.github.com>
2026-06-05 19:56:03 -0700

    Update Features.ts

--- src/shared/Features.ts
+++ src/shared/Features.ts
@@ -6,15 +6,18 @@ import { z } from "zod";
 export const zFeatures = z.object({
   // Organizations
   orgs: z.boolean().optional(),
+
   // Relational mentorship. When disabled, only transactional mentors are supported.
   relational: z.boolean().optional(),
+
   volunteers: z.boolean().optional(),
   interviews: z.boolean().optional(),
   exams: z.boolean().optional(),
+
   /**
    * Controls features related to the mentee's self-filled profile, including:
    * - Prompting mentees to complete their profile via a modal after login, when attempting to book a mentor, or when drafting a mentor selection.
-   * - Showing the "Student's self-filled information" tab on the mentee details page.
+   * - Showing the "学生自填信息" tab on the mentee details page.
    */
   menteeProfile: z.boolean().optional(),
 });
