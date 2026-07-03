addfde1cd09680bdfe1ff092eac2986400b821b3
Weihan Wang <weihanwang@users.noreply.github.com>
2026-07-03 11:38:29 -0700

    Use native localeCompare for compareChinese

--- src/shared/strings/compareChinese.ts
+++ src/shared/strings/compareChinese.ts
@@ -1,8 +1,3 @@
-import { toPinyin } from "./toPinyin";
-
-// Need to convert it to pinyin, otherwise the result
-// will not be correct if compare Chinese directly. Ref:
-// https://www.leevii.com/2023/04/about-the-inaccurate-chinese-sorting-of-localecompare.html
-export function compareChinese(s1: string | null, s2: string | null) {
-  return toPinyin(s1 || "").localeCompare(toPinyin(s2 || ""));
+export function compareChinese(s1: string | null, s2: string | null): number {
+  return (s1 || "").localeCompare(s2 || "", "zh-CN");
 }
