e8c8bbb8a70342210b23182bb9d4bf5d796c5927
Weihan Wang <weihanwang@users.noreply.github.com>
2026-07-10 18:51:31 -0700

    Update migration.ts

--- src/api/routes/migration.ts
+++ src/api/routes/migration.ts
@@ -27,9 +27,5 @@ async function migrateSchema() {
 async function migrateData() {
   console.log("Migrating DB data...");
 
-  await sequelize.query(
-    `UPDATE "Projects" SET visibility = '未列出' WHERE visibility = '保密'`,
-  );
-
   await Promise.resolve();
 }
